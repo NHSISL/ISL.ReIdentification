a5c0c8d3d476a3df6209165107d95f86eedab97f
Joe Parkes <joe.owens@answerdigital.com>
2024-10-17 16:39:55 +0100

    ShouldThrowDependencyValidationExceptionOnProcessCsvIdentificationRequestAndLogItAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.Exceptions.ProcessCsvIdentificationRequests.cs
@@ -0,0 +1,66 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Coordinations.Identifications.Exceptions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using Moq;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifications
+{
+    public partial class IdentificationCoordinationTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnProcessCsvIdentificationRequestAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            Guid someCsvIdentificationRequestId = Guid.NewGuid();
+
+            this.persistanceOrchestrationServiceMock.Setup(service =>
+                service.RetrieveCsvIdentificationRequestByIdAsync(someCsvIdentificationRequestId))
+                    .ThrowsAsync(dependencyValidationException);
+
+            var expectedIdentificationCoordinationDependencyValidationException =
+                new IdentificationCoordinationDependencyValidationException(
+                    message: "Identification coordination dependency validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            // when
+            ValueTask<AccessRequest> accessRequestTask =
+                this.identificationCoordinationService
+                    .ProcessCsvIdentificationRequestAsync(someCsvIdentificationRequestId);
+
+            IdentificationCoordinationDependencyValidationException
+                actualIdentificationCoordinationDependencyValidationException =
+                    await Assert.ThrowsAsync<IdentificationCoordinationDependencyValidationException>(
+                        testCode: accessRequestTask.AsTask);
+
+            // then
+            actualIdentificationCoordinationDependencyValidationException
+                .Should().BeEquivalentTo(expectedIdentificationCoordinationDependencyValidationException);
+
+            this.persistanceOrchestrationServiceMock.Verify(service =>
+                service.RetrieveCsvIdentificationRequestByIdAsync(someCsvIdentificationRequestId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedIdentificationCoordinationDependencyValidationException))),
+                       Times.Once);
+
+            this.persistanceOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.csvHelperBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+            this.accessOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.identificationOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
