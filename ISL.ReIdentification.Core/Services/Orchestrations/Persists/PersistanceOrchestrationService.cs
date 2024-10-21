31e68552c93a78f19cd2622f89399570994d0b1f
Joe Parkes <joe.owens@answerdigital.com>
2024-10-21 17:17:00 +0100

    ShouldStoreRequestAndEmailWhenPersistNewImpersonationContextAsync -> PASS

--- ISL.ReIdentification.Core/Services/Orchestrations/Persists/PersistanceOrchestrationService.cs
+++ ISL.ReIdentification.Core/Services/Orchestrations/Persists/PersistanceOrchestrationService.cs
@@ -3,9 +3,11 @@
 // ---------------------------------------------------------
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Models.Foundations.CsvIdentificationRequests;
+using ISL.ReIdentification.Core.Models.Foundations.ImpersonationContexts;
 using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
 using ISL.ReIdentification.Core.Services.Foundations.CsvIdentificationRequests;
 using ISL.ReIdentification.Core.Services.Foundations.ImpersonationContexts;
@@ -32,8 +34,30 @@ namespace ISL.ReIdentification.Core.Services.Orchestrations.Persists
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<AccessRequest> PersistImpersonationContextAsync(AccessRequest accessRequest) =>
-            throw new NotImplementedException();
+        public async ValueTask<AccessRequest> PersistImpersonationContextAsync(AccessRequest accessRequest)
+        {
+            var maybeImpersonationContexts = await this.impersonationContextService
+                .RetrieveAllImpersonationContextsAsync();
+
+            IQueryable<ImpersonationContext> matchedImpersonationContexts = maybeImpersonationContexts
+                .Where(impersonationContext =>
+                    impersonationContext.Id == accessRequest.ImpersonationContext.Id);
+
+            if (matchedImpersonationContexts.Any())
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                var createdImpersonationContext = await this.impersonationContextService
+                    .AddImpersonationContextAsync(accessRequest.ImpersonationContext);
+
+                var returnedAccessRequest = new AccessRequest { ImpersonationContext = createdImpersonationContext };
+                await this.notificationService.SendPendingApprovalNotificationAsync(returnedAccessRequest);
+
+                return returnedAccessRequest;
+            }
+        }
 
         public ValueTask<AccessRequest> PersistCsvIdentificationRequestAsync(AccessRequest accessRequest) =>
             throw new NotImplementedException();
