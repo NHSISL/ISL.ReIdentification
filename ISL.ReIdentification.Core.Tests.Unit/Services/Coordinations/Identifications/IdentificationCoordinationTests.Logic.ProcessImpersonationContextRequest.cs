26a5b3911ea70e6fc5a0504d30fca31919356b52
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-22 14:23:43 +0100

    ShouldProcessImpersonationContextRequestAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.Logic.ProcessImpersonationContextRequest.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifications
+{
+    public partial class IdentificationCoordinationTests
+    {
+        [Fact]
+        public async Task ShouldProcessImpersonationContextRequestAsync()
+        {
+            // given
+            AccessRequest inputAccessRequest = CreateRandomAccessRequest(); ;
+            AccessRequest outputAccessOrchestrationAccessRequest = CreateRandomAccessRequest();
+            outputAccessOrchestrationAccessRequest.CsvIdentificationRequest = null;
+            outputAccessOrchestrationAccessRequest.IdentificationRequest = null;
+            AccessRequest expectedAccessRequest = outputAccessOrchestrationAccessRequest.DeepClone();
+
+            this.accessOrchestrationServiceMock.Setup(service =>
+                service.ProcessImpersonationContextRequestAsync(inputAccessRequest))
+                    .ReturnsAsync(outputAccessOrchestrationAccessRequest);
+
+            // when
+            AccessRequest actualAccessRequest =
+                await identificationCoordinationService
+                    .ProcessImpersonationContextRequestAsync(inputAccessRequest);
+
+            // then
+            actualAccessRequest.Should().BeEquivalentTo(expectedAccessRequest);
+
+            this.accessOrchestrationServiceMock.Verify(service =>
+                service.ProcessImpersonationContextRequestAsync(inputAccessRequest),
+                    Times.Once);
+
+            this.persistanceOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.accessOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.identificationOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.securityBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
