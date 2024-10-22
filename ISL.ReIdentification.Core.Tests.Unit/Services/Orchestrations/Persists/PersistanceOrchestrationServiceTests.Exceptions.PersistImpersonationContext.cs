533a986874faf54830662f1eea5e56bae266dd09
Joe Parkes <joe.owens@answerdigital.com>
2024-10-22 13:47:52 +0100

    ShouldThrowDependencyValidationOnPersistImpersonationContextAndLogItAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Persists/PersistanceOrchestrationServiceTests.Exceptions.PersistImpersonationContext.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Models.Orchestrations.Persists.Exceptions;
+using Moq;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Persists
+{
+    public partial class PersistanceOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnPersistImpersonationContextAndLogItAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            AccessRequest someAccessRequest = CreateRandomAccessRequest();
+
+            this.impersonationContextServiceMock.Setup(service =>
+                service.RetrieveAllImpersonationContextsAsync())
+                    .Throws(dependencyValidationException);
+
+            var expectedPersistanceOrchestrationDependencyValidationException =
+                new PersistanceOrchestrationDependencyValidationException(
+                    message: "Persistance orchestration dependency validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyValidationException.InnerException as Xeption);
+
+            // when
+            ValueTask<AccessRequest> persistImpersonationContextAsyncTask =
+                this.persistanceOrchestrationService.PersistImpersonationContextAsync(
+                    accessRequest: someAccessRequest);
+
+            PersistanceOrchestrationDependencyValidationException
+                actualPersistanceOrchestrationDependencyValidationException =
+                await Assert.ThrowsAsync<PersistanceOrchestrationDependencyValidationException>(
+                    testCode: persistImpersonationContextAsyncTask.AsTask);
+
+            // then
+            actualPersistanceOrchestrationDependencyValidationException
+                .Should().BeEquivalentTo(expectedPersistanceOrchestrationDependencyValidationException);
+
+            this.impersonationContextServiceMock.Verify(service =>
+                service.RetrieveAllImpersonationContextsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedPersistanceOrchestrationDependencyValidationException))),
+                       Times.Once);
+
+            this.impersonationContextServiceMock.VerifyNoOtherCalls();
+            this.csvIdentificationRequestServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.impersonationContextServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+            this.hashBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
