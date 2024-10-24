10f034fcf00e36d82b303e794fb8228cbbabca33
Joe Parkes <joe.owens@answerdigital.com>
2024-10-24 14:46:19 +0100

    ShouldRemoveRecordOnDeleteByIdsAsync -> PASS

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.Logic.Delete.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.Logic.Delete.cs
@@ -4,19 +4,18 @@
 
 using System;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Force.DeepCloner;
 using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
-using ISL.ReIdentification.Core.Models.Foundations.PdsDatas.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RESTFulSense.Clients.Extensions;
 
 namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsDatas
 {
     public partial class PdsDataControllerTests
     {
         [Fact]
-        public async Task DeletePdsDataByIdAsyncShouldReturnPdsData()
+        public async Task ShouldRemoveRecordOnDeleteByIdsAsync()
         {
             // given
             PdsData randomPdsData = CreateRandomPdsData();
@@ -24,42 +23,28 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsD
             PdsData storagePdsData = randomPdsData;
             PdsData expectedPdsData = storagePdsData.DeepClone();
 
-            pdsDataServiceMock.Setup(service =>
-                service.RemovePdsDataByIdAsync(inputId))
-                .ReturnsAsync(storagePdsData);
+            var expectedObjectResult =
+                new OkObjectResult(expectedPdsData);
 
-            // when
-            var result = await this.pdsDataController.DeletePdsDataByIdAsync(inputId);
-
-            // then
-            var actualResult = Assert.IsType<OkObjectResult>(result.Result);
-            actualResult.StatusCode.Should().Be(200);
-            actualResult.Value.Should().BeEquivalentTo(expectedPdsData);
-        }
-
-        [Fact]
-        public async Task
-            DeletePdsDataByIdAsyncShouldReturnNotFoundWhenPdsDataValidationExceptionOccurs()
-        {
-            // given
-            Guid randomId = Guid.NewGuid();
-            Guid inputId = randomId;
-            var notFoundPdsDataException = new NotFoundPdsDataException(message: inputId.ToString());
-
-            var delegatedAccessValidationException = new PdsDataValidationException(
-                message: GetRandomString(),
-                innerException: notFoundPdsDataException);
+            var expectedActionResult =
+                new ActionResult<PdsData>(expectedObjectResult);
 
             pdsDataServiceMock
-                .Setup(service => service.RemovePdsDataByIdAsync(inputId))
-                .ThrowsAsync(delegatedAccessValidationException);
+                .Setup(service => service.RemovePdsDataByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(storagePdsData);
 
             // when
-            var result = await pdsDataController.DeletePdsDataByIdAsync(inputId);
+            ActionResult<PdsData> actualActionResult =
+                await pdsDataController.DeletePdsDataByIdAsync(inputId);
 
             // then
-            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result.Result);
-            notFoundObjectResult.StatusCode.Should().Be(404);
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            pdsDataServiceMock
+                .Verify(service => service.RemovePdsDataByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            pdsDataServiceMock.VerifyNoOtherCalls();
         }
     }
 }
