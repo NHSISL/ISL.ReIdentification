768981445d66849be00115ac946affdbcff79e49
Joe Parkes <joe.owens@answerdigital.com>
2024-10-24 15:45:30 +0100

    ShouldReturnBadRequestOnDeleteIfValidationErrorOccurredAsync -> FAIL

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.Exceptions.Delete.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+
+namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsDatas
+{
+    public partial class PdsDataControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnDeleteIfValidationErrorOccurredAsync(Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<PdsData>(expectedBadRequestObjectResult);
+
+            this.pdsDataServiceMock.Setup(service =>
+                service.RemovePdsDataByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<PdsData> actualActionResult =
+                await this.pdsDataController.DeletePdsDataByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.pdsDataServiceMock.Verify(service =>
+                service.RemovePdsDataByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.pdsDataServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Unit/Controllers/PdsDatas/PdsDataControllerTests.cs
@@ -5,10 +5,12 @@
 using System;
 using ISL.ReIdentification.Configurations.Server.Controllers;
 using ISL.ReIdentification.Core.Models.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.PdsDatas.Exceptions;
 using ISL.ReIdentification.Core.Services.Foundations.PdsDatas;
 using Moq;
 using RESTFulSense.Controllers;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsDatas
 {
@@ -21,6 +23,23 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Unit.Controllers.PdsD
         {
             pdsDataServiceMock = new Mock<IPdsDataService>();
             pdsDataController = new PdsDataController(pdsDataServiceMock.Object);
+        }
+
+        public static TheoryData<Xeption> ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = GetRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new PdsDataValidationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new PdsDataDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
         }
 
         private static string GetRandomString() =>
