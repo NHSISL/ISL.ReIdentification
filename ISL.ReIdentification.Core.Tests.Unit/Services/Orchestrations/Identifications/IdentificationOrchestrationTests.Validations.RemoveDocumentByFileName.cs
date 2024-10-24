e78f9a1fd145c0279afff70d40a3143f092e3a4d
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-24 16:59:01 +0100

    ShouldThrowValidationExceptionOnRemoveDocumentByFileNameWhenArgumentsInvalidAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Identifications/IdentificationOrchestrationTests.Validations.RemoveDocumentByFileName.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Identifications.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Identifications
+{
+    public partial class IdentificationOrchestrationTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRemoveDocumentByFileNameWhenArgumentsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidArgumentIdentificationOrchestrationException =
+            new InvalidArgumentIdentificationOrchestrationException(
+                    message: "Invalid argument identification orchestration exception, " +
+                        "please correct the errors and try again.");
+
+            invalidArgumentIdentificationOrchestrationException.AddData(
+                key: "fileName",
+                values: "Text is invalid");
+
+            invalidArgumentIdentificationOrchestrationException.AddData(
+                key: "container",
+                values: "Text is invalid");
+
+            var expectedIdentificationOrchestrationValidationException =
+                new IdentificationOrchestrationValidationException(
+                    message: "Identification orchestration validation error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: invalidArgumentIdentificationOrchestrationException);
+
+            // when
+            ValueTask identificationRequestTask =
+                this.identificationOrchestrationService
+                    .RemoveDocumentByFileNameAsync(invalidString, invalidString);
+
+            IdentificationOrchestrationValidationException
+                actualIdentificationOrchestrationValidationException =
+                    await Assert.ThrowsAsync<IdentificationOrchestrationValidationException>(
+                        testCode: identificationRequestTask.AsTask);
+
+            // then
+            actualIdentificationOrchestrationValidationException
+                .Should().BeEquivalentTo(expectedIdentificationOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedIdentificationOrchestrationValidationException))),
+                       Times.Once);
+
+            this.accessAuditServiceMock.VerifyNoOtherCalls();
+            this.reIdentificationServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
