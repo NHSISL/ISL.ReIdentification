28c8db0e04304f50172128f299dfcf84ff3d48b1
David Cunliffe <david.cunliffe2@nhs.net>
2024-10-24 14:12:50 +0100

    coderub: adding logging

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/vite.config.ts
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/vite.config.ts
@@ -16,6 +16,9 @@ const certificateName = "isl.reidentification.configurations.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
+console.log(certFilePath);
+console.log(keyFilePath);
+
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     if (0 !== child_process.spawnSync('dotnet', [
         'dev-certs',
