8ac14755d2fbd71e90c28fe8ef45f099553df1a8
Joe Parkes <joe.owens@answerdigital.com>
2024-10-28 16:03:25 +0000

    ShouldPutPdsDataAsync -> FAIL

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Brokers/ApiBroker.PdsData.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Brokers/ApiBroker.PdsData.cs
@@ -23,6 +23,9 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Brokers
         public async ValueTask<PdsData> GetPdsDataByIdAsync(Guid pdsDataId) =>
             await this.apiFactoryClient.GetContentAsync<PdsData>($"{PdsDataRelativeUrl}/{pdsDataId}");
 
+        public async ValueTask<PdsData> PutPdsDataAsync(PdsData pdsData) =>
+            throw new NotImplementedException();
+
         public async ValueTask<PdsData> DeletePdsDataByIdAsync(Guid pdsDataId) =>
             await this.apiFactoryClient.DeleteContentAsync<PdsData>(
                 $"{PdsDataRelativeUrl}/{pdsDataId}");
