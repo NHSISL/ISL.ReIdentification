b7858bf82ce37a6a90c231ee6bb004bcb1fabd16
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-10-28 11:51:03 +0000

    ShouldThrowDependencyOnAddDocumentAndLogItAsync -> PASS

--- ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Identifications/IdentificationOrchestrationTests.Exceptions.AddDocument.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Identifications/IdentificationOrchestrationTests.Exceptions.AddDocument.cs
@@ -64,6 +64,54 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Identific
             this.identifierBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Theory]
+        [MemberData(nameof(DocumentDependencyExceptions))]
+        public async Task ShouldThrowDependencyOnAddDocumentAndLogItAsync(Xeption dependencyException)
+        {
+            // given
+            string someFilename = GetRandomString();
+            string someContainer = GetRandomString();
+            Stream someStream = new HasLengthStream();
 
+            this.documentServiceMock.Setup(service =>
+                service.AddDocumentAsync(someStream, someFilename, someContainer))
+                    .ThrowsAsync(dependencyException);
+
+            var expectedIdentificationOrchestrationDependencyException =
+                new IdentificationOrchestrationDependencyException(
+                    message: "Identification orchestration dependency error occurred, " +
+                        "fix the errors and try again.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            // when
+            ValueTask addDocumentTask =
+                this.identificationOrchestrationService
+                    .AddDocumentAsync(someStream, someFilename, someContainer);
+
+            IdentificationOrchestrationDependencyException
+                actualIdentificationOrchestrationDependencyException =
+                    await Assert.ThrowsAsync<IdentificationOrchestrationDependencyException>(
+                        testCode: addDocumentTask.AsTask);
+
+            // then
+            actualIdentificationOrchestrationDependencyException
+                .Should().BeEquivalentTo(expectedIdentificationOrchestrationDependencyException);
+
+            this.documentServiceMock.Verify(service =>
+                service.AddDocumentAsync(someStream, someFilename, someContainer),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedIdentificationOrchestrationDependencyException))),
+                       Times.Once);
+
+            this.documentServiceMock.VerifyNoOtherCalls();
+            this.accessAuditServiceMock.VerifyNoOtherCalls();
+            this.reIdentificationServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
