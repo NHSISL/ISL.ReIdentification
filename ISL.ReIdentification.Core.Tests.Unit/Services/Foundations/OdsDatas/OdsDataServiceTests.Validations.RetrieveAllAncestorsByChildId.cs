a200fe7bf2bddb7e7d26d195b2b57305af89446b
Joe Parkes <joe.owens@answerdigital.com>
2024-10-28 14:32:15 +0000

    ShouldThrowValidationExceptionOnRetrieveAllAncestorsByChildIdIsInvalidAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/OdsDatas/OdsDataServiceTests.Validations.RetrieveAllAncestorsByChildId.cs
@@ -0,0 +1,61 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.OdsDatas
+{
+    public partial class OdsDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveAllAncestorsByChildIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidOdsDataId = Guid.Empty;
+
+            var invalidOdsDataException =
+                new InvalidOdsDataException(
+                    message: "Invalid odsData. Please correct the errors and try again.");
+
+            invalidOdsDataException.AddData(
+                key: nameof(OdsData.Id),
+                values: "Id is invalid");
+
+            var expectedOdsDataValidationException =
+                new OdsDataValidationException(
+                    message: "OdsData validation error occurred, please fix errors and try again.",
+                    innerException: invalidOdsDataException);
+
+            // when
+            ValueTask<List<OdsData>> retrieveOdsDataByIdTask =
+                this.odsDataService.RetrieveAllAncestorsByChildId(invalidOdsDataId);
+
+            OdsDataValidationException actualOdsDataValidationException =
+                await Assert.ThrowsAsync<OdsDataValidationException>(
+                    testCode: retrieveOdsDataByIdTask.AsTask);
+
+            // then
+            actualOdsDataValidationException.Should()
+                .BeEquivalentTo(expectedOdsDataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedOdsDataValidationException))),
+                        Times.Once);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.SelectAllOdsDatasAsync(),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+        }
+    }
+}
