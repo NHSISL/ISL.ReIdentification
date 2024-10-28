9e226daed5494782c4e9c20266e34b27def58d84
Christo du Toit <christo@dutoit.co.uk>
2024-10-28 12:04:38 +0000

    CODE RUB: Code cleanup

--- ISL.ReIdentification.Core/Services/Orchestrations/Identifications/IdentificationOrchestrationService.cs
+++ ISL.ReIdentification.Core/Services/Orchestrations/Identifications/IdentificationOrchestrationService.cs
@@ -77,7 +77,8 @@ namespace ISL.ReIdentification.Core.Services.Orchestrations.Identifications
                 if (item.HasAccess is false)
                 {
                     item.Identifier = "0000000000";
-                    item.Message = "Failed to Re-Identify. User do not have access to the organisation(s) associated with patient.";
+                    item.Message = "Failed to Re-Identify. User do not have access to the organisation(s) " + 
+                        "associated with patient.";
                 }
             }
 
