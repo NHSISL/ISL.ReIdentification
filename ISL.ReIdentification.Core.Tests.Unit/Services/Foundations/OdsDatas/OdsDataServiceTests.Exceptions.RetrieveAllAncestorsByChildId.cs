7fe98c81d528235fc6fc0189931c3d559a792d72
Joe Parkes <joe.owens@answerdigital.com>
2024-10-28 14:41:07 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllAncestorsByChildIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/OdsDatas/OdsDataServiceTests.Exceptions.RetrieveAllAncestorsByChildId.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas;
+using ISL.ReIdentification.Core.Models.Foundations.OdsDatas.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.OdsDatas
+{
+    public partial class OdsDataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllAncestorsByChildIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageOdsDataException =
+                new FailedStorageOdsDataException(
+                    message: "Failed odsData storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedOdsDataDependencyException =
+                new OdsDataDependencyException(
+                    message: "OdsData dependency error occurred, contact support.",
+                    innerException: failedStorageOdsDataException);
+
+            this.reIdentificationStorageBroker.Setup(broker =>
+                broker.SelectOdsDataByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<List<OdsData>> retrieveAncestorsByChildIdTask =
+                this.odsDataService.RetrieveAllAncestorsByChildId(someId);
+
+            OdsDataDependencyException actualOdsDataDependencyException =
+                await Assert.ThrowsAsync<OdsDataDependencyException>(
+                    testCode: retrieveAncestorsByChildIdTask.AsTask);
+
+            // then
+            actualOdsDataDependencyException.Should()
+                .BeEquivalentTo(expectedOdsDataDependencyException);
+
+            this.reIdentificationStorageBroker.Verify(broker =>
+                broker.SelectOdsDataByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedOdsDataDependencyException))),
+                        Times.Once);
+
+            this.reIdentificationStorageBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
