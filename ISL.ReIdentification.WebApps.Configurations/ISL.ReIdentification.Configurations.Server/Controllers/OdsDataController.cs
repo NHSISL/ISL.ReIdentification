e8137cc27c5f015d7ab7406b3bf3e25e5d50e8a2
Joe Parkes <joe.owens@answerdigital.com>
2024-10-28 15:17:17 +0000

    ShouldReturnInternalServerErrorOnGetAllAncestorsIfServerErrorOccurredAsync -> PASS

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server/Controllers/OdsDataController.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server/Controllers/OdsDataController.cs
@@ -101,9 +101,20 @@ namespace ISL.ReIdentification.Configurations.Server.Controllers
         [HttpGet("GetAncestors")]
         public async ValueTask<ActionResult<List<OdsData>>> GetAllAncestors(Guid id)
         {
-            List<OdsData> retrievedOdsDatas = await this.odsDataService.RetrieveAllAncestorsByChildId(id);
+            try
+            {
+                List<OdsData> retrievedOdsDatas = await this.odsDataService.RetrieveAllAncestorsByChildId(id);
 
-            return Ok(retrievedOdsDatas);
+                return Ok(retrievedOdsDatas);
+            }
+            catch (OdsDataDependencyException odsDataDependencyException)
+            {
+                return InternalServerError(odsDataDependencyException);
+            }
+            catch (OdsDataServiceException odsDataServiceException)
+            {
+                return InternalServerError(odsDataServiceException);
+            }
         }
 
         [HttpGet("{odsDataId}")]
