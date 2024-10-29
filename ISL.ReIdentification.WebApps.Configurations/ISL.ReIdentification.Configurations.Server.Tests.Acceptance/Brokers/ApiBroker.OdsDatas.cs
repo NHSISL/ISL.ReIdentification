d7ded36673826c3d72705166bd325633f48534ef
Joe Parkes <joe.owens@answerdigital.com>
2024-10-29 09:20:51 +0000

    ShouldPostOdsDataAsync -> FAIL

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Brokers/ApiBroker.OdsDatas.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Brokers/ApiBroker.OdsDatas.cs
@@ -13,10 +13,16 @@ namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Brokers
     {
         private const string odsDataRelativeUrl = "api/odsdata";
 
+        public async ValueTask<OdsData> PostOdsDataAsync(OdsData odsData) =>
+            throw new NotImplementedException();
+
         public async ValueTask<List<OdsData>> GetAllOdsDatasAsync() =>
             await this.apiFactoryClient.GetContentAsync<List<OdsData>>($"{odsDataRelativeUrl}");
 
         public async ValueTask<OdsData> GetOdsDataByIdAsync(Guid odsDataId) =>
             await this.apiFactoryClient.GetContentAsync<OdsData>($"{odsDataRelativeUrl}/{odsDataId}");
+
+        public async ValueTask<OdsData> DeleteOdsDataByIdAsync(Guid odsDataId) =>
+            await this.apiFactoryClient.DeleteContentAsync<OdsData>($"{odsDataRelativeUrl}/{odsDataId}");
     }
 }
--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Models/OdsDatas/OdsData.cs
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Models/OdsDatas/OdsData.cs
@@ -3,11 +3,18 @@
 // ---------------------------------------------------------
 
 using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Models.OdsDatas
 {
     public class OdsData
     {
         public Guid Id { get; set; }
+        public HierarchyId OdsHierarchy { get; set; }
+        public string OrganisationCode { get; set; }
+        public string OrganisationName { get; set; }
+        public DateTimeOffset? RelationshipWithParentStartDate { get; set; }
+        public DateTimeOffset? RelationshipWithParentEndDate { get; set; }
+        public bool HasChildren { get; set; }
     }
 }
