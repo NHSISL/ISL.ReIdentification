3da95a49b0f9470f45062e2509b0b5f6e9712738
MichaelOSullivanAnswer <116073025+MichaelOSullivanAnswer@users.noreply.github.com>
2024-10-30 14:41:39 +0000

    CODE RUB: Code cleanup
    Co-authored-by: Christo du Toit <christo@dutoit.co.uk>

--- ISL.ReIdentification.Core/Services/Foundations/Notifications/NotificationService.cs
+++ ISL.ReIdentification.Core/Services/Foundations/Notifications/NotificationService.cs
@@ -239,7 +239,7 @@ namespace ISL.ReIdentification.Core.Services.Foundations.Notifications
                 { "reason", accessRequest.CsvIdentificationRequest.Reason },
                 { "organisation", accessRequest.CsvIdentificationRequest.Organisation },
                 { "identifierColumn", accessRequest.CsvIdentificationRequest.IdentifierColumn },
-                { "templateId", this.notificationConfigurations.CsvPendingApprovalRequestTemplateId },
+                { "templateId", this.notificationConfigurations.ImpersonationDeniedRequestTemplateId },
                 { "configurationBaseUrl", this.notificationConfigurations.ConfigurationBaseUrl },
                 { "portalBaseUrl", this.notificationConfigurations.PortalBaseUrl },
             };
