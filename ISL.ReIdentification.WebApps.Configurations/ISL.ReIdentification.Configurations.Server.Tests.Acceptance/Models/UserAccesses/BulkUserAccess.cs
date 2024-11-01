bee2ffb6d42ada4f478c47f8c0f1ff8bc6e9a325
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-01 12:00:56 +0000

    CODE RUB: Code cleanup

---
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Server.Tests.Acceptance/Models/UserAccesses/BulkUserAccess.cs
@@ -0,0 +1,14 @@
+﻿namespace ISL.ReIdentification.Configurations.Server.Tests.Acceptance.Models.UserAccesses
+{
+    public class BulkUserAccess
+    {
+        public Guid EntraUserId { get; set; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string DisplayName { get; set; }
+        public string JobTitle { get; set; }
+        public string Email { get; set; }
+        public string UserPrincipalName { get; set; }
+        public List<string> OrgCodes { get; set; }
+    }
+}
