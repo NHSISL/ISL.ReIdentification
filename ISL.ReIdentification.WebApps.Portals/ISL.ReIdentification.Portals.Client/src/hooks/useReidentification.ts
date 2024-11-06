f578a16a50db58aa9012c2a49033875ee82b168a
David Cunliffe <david.cunliffe2@nhs.net>
2024-11-06 17:59:56 +0000

    wip: reidentification caching service

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/hooks/useReidentification.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/hooks/useReidentification.ts
@@ -1,6 +1,6 @@
 import { useMsal } from "@azure/msal-react";
 import { AccessRequest } from "../models/accessRequest/accessRequest";
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 import { ReIdRecord } from "../types/ReIdRecord";
 import { reIdentificationService } from "../services/foundations/reIdentificationService";
 import { IdentificationItem } from "../models/ReIdentifications/IdentificationItem";
@@ -24,66 +24,17 @@ export function useReidentification(reason: string) {
     const [isLoading, setIsLoading] = useState(false);
     const [lastPseudo, setLastPseudo] = useState<ReIdRecord>();
     const { accounts } = useMsal();
-    const { submit } = reIdentificationService.useRequestReIdentification();
+    const { submit, loading, data } = reIdentificationService.useRequestReIdentification();
 
-    function isHx(hxNumber: string) {
-        return hxNumber.indexOf("-") !== -1
-    }
-
-    function convertHx(hxNumber: string) {
-        const cleanedID = hxNumber.replace(/-/g, '');
-
-        const originalHex = cleanedID.split('').reverse().join('');
-
-        return parseInt(originalHex, 16).toString();
-    }
+    useEffect(() => {
+        console.log(data);
+    },[data])
 
     const processRequest = async (request: TrackedAccessRequest) => {
 
         if (request.accessRequest.identificationRequest) {
-
-            setReidentifications((reidentifications) => {
-                const nextX = [...reidentifications];
-                request.accessRequest.identificationRequest?.identificationItems.forEach(x => {
-                    const origId = savedIdentificationItems.find(ii => ii.rowNumber === x.rowNumber)?.identifier || "";
-                    nextX.unshift({
-                        identifier: x.identifier,
-                        pseudo: origId || "",
-                        hasAccess: x.hasAccess,
-                        nhsnumber: "",
-                        loading: true,
-                        rowNumber: x.rowNumber,
-                        isHx: isHx(origId)
-                    })
-                }
-                )
-                return nextX;
-            });
-
-            const savedIdentificationItems = [...request.accessRequest.identificationRequest.identificationItems]
-            request.accessRequest.identificationRequest.identificationItems = request.accessRequest.identificationRequest.identificationItems.map(ii => {
-                return {
-                    ...ii,
-                    identifier: `0000000000000${isHx(ii.identifier) ? convertHx(ii.identifier) : ii.identifier}`.slice(-10),
-                }
-            });
-
             setIsLoading(true);
-            const result = await submit(request.accessRequest);
-
-            setReidentifications((reidentifications) => {
-                const nextX = [...reidentifications];
-                result.identificationRequest?.identificationItems.forEach(x => {
-                    const origId = savedIdentificationItems.find(ii => ii.rowNumber === x.rowNumber)?.identifier;
-
-                    if (origId) {
-                        const index = nextX.findIndex(filter => filter.pseudo === origId);
-                        nextX[index] = { ...nextX[index], nhsnumber: x.identifier, loading: false, hasAccess: x.hasAccess }
-                    }
-                })
-                return nextX;
-            });
-
+            await submit(request.accessRequest);
             setIsLoading(false);
         }
     }
@@ -93,7 +44,7 @@ export function useReidentification(reason: string) {
 
         const identificationItems = pseudoNumbers.map((ps, i): IdentificationItem => {
             return {
-                rowNumber: "" + i,
+                rowNumber: crypto.randomUUID(),
                 identifier: ps,
                 hasAccess: false
             }
@@ -124,7 +75,7 @@ export function useReidentification(reason: string) {
         setReidentifications([]);
     }
 
-    return { reidentify, reidentifications, lastPseudo, clearList, isLoading }
+    return { reidentify, reidentifications: data, lastPseudo, clearList, isLoading }
 }
 
 
--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/services/foundations/reIdentificationService.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/services/foundations/reIdentificationService.ts
@@ -1,22 +1,106 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReIdentificationBroker from '../../brokers/apiBroker.reidentification';
 import { AccessRequest } from '../../models/accessRequest/accessRequest';
+import { IdentificationItem } from '../../models/ReIdentifications/IdentificationItem';
+import { ReIdRecord } from '../../types/ReIdRecord';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 export const reIdentificationService = {
-    
+
+
 
     useRequestReIdentification: () => {
         const broker = new ReIdentificationBroker();
         const [loading, setIsLoading] = useState(false);
+        const [request, setRequest] = useState<AccessRequest>({});
+        const [data, setData] = useState<ReIdRecord[]>([]);
+
+        function isHx(hxNumber: string) {
+            return hxNumber.indexOf("-") !== -1
+        }
+
+        function convertHx(hxNumber: string) {
+            const cleanedID = hxNumber.replace(/-/g, '');
+    
+            const originalHex = cleanedID.split('').reverse().join('');
+    
+            return parseInt(originalHex, 16).toString();
+        }
+
+        useEffect(() => {
+
+            async function execute() {
+                if (request && request.identificationRequest && request.identificationRequest?.identificationItems) {
+                    setIsLoading(true);
+
+                    console.log(request.identificationRequest.identificationItems.length);
+                    const reIdRecords: ReIdRecord[] = request.identificationRequest?.identificationItems.map(ii => {
+                        return {
+                            identifier: isHx(ii.identifier) ? convertHx(ii.identifier) : ii.identifier,
+                            pseudo: ii.identifier,
+                            hasAccess: ii.hasAccess,
+                            nhsnumber: "",
+                            loading: true,
+                            rowNumber: ii.rowNumber,
+                            isHx: isHx(ii.identifier)
+                        }
+                    })
+
+                    request.identificationRequest.identificationItems = 
+                            request.identificationRequest.identificationItems.map(i => 
+                                    {return { ...i, 
+                                        identifier: `0000000000000${isHx(i.identifier) ? convertHx(i.identifier) : i.identifier}`.slice(-10) }});
+
+                    request.identificationRequest.identificationItems = request.identificationRequest.identificationItems.filter(x => data.findIndex(y => y.identifier === x.identifier) === -1);
+                    
+                    if(request.identificationRequest.identificationItems.length === 0){
+                        return;
+                    }
+
+                    
+                    await broker.PostReIdentificationAsync(request)
+                        .then((response) => {
+                            setData((data) => {
+                                const responseItems = response.identificationRequest?.identificationItems;
+                                if(!responseItems) {
+                                    return data;
+                                }
+
+                                const foo : ReIdRecord[] =  responseItems.map(x => {
+                                    const r = reIdRecords.find(cachedRecord => cachedRecord.rowNumber === x.rowNumber);
+                                    return { ...r, 
+                                        hasAccess: x.hasAccess,
+                                        nhsnumber: x.identifier,
+                                        loading: false,
+                                    }
+                                })
+
+                                console.log(reIdRecords);
+                                console.log(foo);
+
+                                return [...data,...foo,]
+
+                            })
+                        }).finally(() => {
+                            setIsLoading(false);
+                        })
+
+
+                }
+            }
+
+            execute();
+
+        }, [request]);
+
+
         return {
             submit: (identificationRequest: AccessRequest) => {
                 setIsLoading(true);
-                return broker.PostReIdentificationAsync(identificationRequest).finally(() =>
-                {
-                    setIsLoading(false);
-                })
+                setRequest(identificationRequest);
             },
-            loading
+            loading,
+            data
         };
     }
 }
