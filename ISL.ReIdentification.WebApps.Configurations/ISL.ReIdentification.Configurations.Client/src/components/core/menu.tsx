da0de958531a1b83d517249425017523f1f3a866
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-08 16:01:38 +0000

    Code Rub: Feature Setup

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/components/core/menu.tsx
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/components/core/menu.tsx
@@ -6,6 +6,8 @@ import { useLocation } from 'react-router-dom';
 import { SecuredLink } from '../securitys/securedLinks';
 import { faUserFriends } from '@fortawesome/free-solid-svg-icons/faUserFriends';
 import { faIdBadge } from '@fortawesome/free-solid-svg-icons/faIdBadge';
+import { FeatureDefinitions } from '../../featureDefinitions';
+import { FeatureSwitch } from '../accessControls/featureSwitch';
 
 const MenuComponent: React.FC = () => {
     const location = useLocation();
@@ -25,47 +27,55 @@ const MenuComponent: React.FC = () => {
                 <SecuredLink to="/home">Home</SecuredLink>
             </ListGroup.Item>
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/userAccess' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/userAccess')}>
-                <FontAwesomeIcon icon={faUser} className="me-2 fa-icon" />
-                <SecuredLink to="/userAccess">User Access</SecuredLink>
-            </ListGroup.Item>
+            <FeatureSwitch feature={FeatureDefinitions.UserAccess}>
+                <ListGroup.Item
+                    className={`bg-dark text-white ${activePath === '/userAccess' ? 'active' : ''}`}
+                    onClick={() => handleItemClick('/userAccess')}>
+                    <FontAwesomeIcon icon={faUser} className="me-2 fa-icon" />
+                    <SecuredLink to="/userAccess">User Access</SecuredLink>
+                </ListGroup.Item>
+            </FeatureSwitch>
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/impersonationContext' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/impersonationContext')}>
-                <FontAwesomeIcon icon={faUserFriends} className="me-2 fa-icon" />
-                <SecuredLink to="/impersonationContext">Impersonation Context</SecuredLink>
-            </ListGroup.Item>
+            {/*<ListGroup.Item*/}
+            {/*    className={`bg-dark text-white ${activePath === '/impersonationContext' ? 'active' : ''}`}*/}
+            {/*    onClick={() => handleItemClick('/impersonationContext')}>*/}
+            {/*    <FontAwesomeIcon icon={faUserFriends} className="me-2 fa-icon" />*/}
+            {/*    <SecuredLink to="/impersonationContext">Impersonation Context</SecuredLink>*/}
+            {/*</ListGroup.Item>*/}
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/csvIdentificationRequest' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/csvIdentificationRequest')}>
-                <FontAwesomeIcon icon={faIdBadge} className="me-2 fa-icon" />
-                <SecuredLink to="/csvIdentificationRequest">Csv Identification Request</SecuredLink>
-            </ListGroup.Item>
+            {/*<ListGroup.Item*/}
+            {/*    className={`bg-dark text-white ${activePath === '/csvIdentificationRequest' ? 'active' : ''}`}*/}
+            {/*    onClick={() => handleItemClick('/csvIdentificationRequest')}>*/}
+            {/*    <FontAwesomeIcon icon={faIdBadge} className="me-2 fa-icon" />*/}
+            {/*    <SecuredLink to="/csvIdentificationRequest">Csv Identification Request</SecuredLink>*/}
+            {/*</ListGroup.Item>*/}
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/pdsData' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/pdsData')}>
-                <FontAwesomeIcon icon={faAddressBook} className="me-2 fa-icon" />
-                <SecuredLink to="/pdsData">PDS Data View</SecuredLink>
-            </ListGroup.Item>
+            <FeatureSwitch feature={FeatureDefinitions.Pds}>
+                <ListGroup.Item
+                    className={`bg-dark text-white ${activePath === '/pdsData' ? 'active' : ''}`}
+                    onClick={() => handleItemClick('/pdsData')}>
+                    <FontAwesomeIcon icon={faAddressBook} className="me-2 fa-icon" />
+                    <SecuredLink to="/pdsData">PDS Data View</SecuredLink>
+                </ListGroup.Item>
+            </FeatureSwitch>
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/odsData' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/odsData')}>
-                <FontAwesomeIcon icon={faUserDoctor} className="me-2 fa-icon" />
-                <SecuredLink to="/odsData">Ods Data View</SecuredLink>
-            </ListGroup.Item>
+            <FeatureSwitch feature={FeatureDefinitions.Ods}>
+                <ListGroup.Item
+                    className={`bg-dark text-white ${activePath === '/odsData' ? 'active' : ''}`}
+                    onClick={() => handleItemClick('/odsData')}>
+                    <FontAwesomeIcon icon={faUserDoctor} className="me-2 fa-icon" />
+                    <SecuredLink to="/odsData">Ods Data View</SecuredLink>
+                </ListGroup.Item>
+            </FeatureSwitch>
 
-            <ListGroup.Item
-                className={`bg-dark text-white ${activePath === '/configuration/home' ? 'active' : ''}`}
-                onClick={() => handleItemClick('/configuration/home')}>
-                <FontAwesomeIcon icon={faCog} className="me-2 fa-icon" />
-                <SecuredLink to="/configuration/home">Configuration</SecuredLink>
-            </ListGroup.Item>
+            <FeatureSwitch feature={FeatureDefinitions.Configuration}>
+                <ListGroup.Item
+                    className={`bg-dark text-white ${activePath === '/configuration/home' ? 'active' : ''}`}
+                    onClick={() => handleItemClick('/configuration/home')}>
+                    <FontAwesomeIcon icon={faCog} className="me-2 fa-icon" />
+                    <SecuredLink to="/configuration/home">Configuration</SecuredLink>
+                </ListGroup.Item>
+            </FeatureSwitch>
         </ListGroup>
     );
 }
