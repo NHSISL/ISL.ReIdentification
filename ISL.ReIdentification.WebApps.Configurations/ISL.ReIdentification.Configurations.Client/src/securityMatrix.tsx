e93c58ec79e5819561cf9c02c39c902b58d1e18e
David Cunliffe <david.l.cunliffe@gmail.com>
2024-11-11 16:21:44 +0000

    coderub: updating security matrixes

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/securityMatrix.tsx
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/securityMatrix.tsx
@@ -6,9 +6,9 @@ const securityPoints = {
         view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
     },
     lookups: {
-        add: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        edit: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
+        add: ['ISL.Reidentification.Configuration.Administrators'],
+        edit: ['ISL.Reidentification.Configuration.Administrators'],
+        delete: ['ISL.Reidentification.Configuration.Administrators'],
         view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
     },
     userAccess: {
@@ -17,30 +17,7 @@ const securityPoints = {
         delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
         view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
     },
-    singleReId: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    csvReId: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    csvWorklist: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    reports: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    }
+
 }
 
 export default securityPoints
--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/securityMatrix.tsx
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/securityMatrix.tsx
@@ -1,51 +1,9 @@
 const securityPoints = {
-    configuration: {
-        add: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        edit: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-    },
-    lookups: {
-        add: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        edit: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-    },
-    userAccess: {
-        add: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        edit: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-        view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Configuration.Users'],
-    },
-    singleReId: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    csvReId: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    csvWorklist: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
-    reports: {
-        add: [],
-        edit: [],
-        delete: [],
-        view: [],
-    },
     impersonation: {
-        add: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Portal.DataEngineers'],
-        edit: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Portal.DataEngineers'],
-        delete: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Portal.DataEngineers'],
-        view: ['ISL.Reidentification.Configuration.Administrators', 'ISL.Reidentification.Portal.DataEngineers'],
+        add: ['ISL.Reidentification.Portal.DataEngineers'],
+        edit: ['ISL.Reidentification.Portal.DataEngineers'],
+        delete: ['ISL.Reidentification.Portal.DataEngineers'],
+        view: ['ISL.Reidentification.Portal.DataEngineers'],
     }
 }
 
