3918aab906b00750979f1bb04614f8a02ff3927a
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-12 14:25:33 +0000

    CODE RUB: Update Features Test

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Server.Tests.Acceptance/Apis/FeaturesApiTests.Logic.cs
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Server.Tests.Acceptance/Apis/FeaturesApiTests.Logic.cs
@@ -20,9 +20,13 @@ namespace ISL.ReIdentification.Portals.Server.Tests.Acceptance.Apis
                 "\r\n  \"UserAccess\"," +
                 "\r\n  \"DelegatedUserAccess\"," +
                 "\r\n  \"Ods\"," +
-                "\r\n  \"Pds\"" +
+                "\r\n  \"Pds\"," +
+                "\r\n  \"ReportReidentify\"," +
+                "\r\n  \"SinglePatientReidentify\"," +
+                "\r\n  \"CsvReidentify\"," +
+                "\r\n  \"Projects\"," +
+                "\r\n  \"CsvWorklist\"" +
                 "\r\n]";
-
             // When
             string actualResult = await this.apiBroker.GetFeaturesAsync();
 
