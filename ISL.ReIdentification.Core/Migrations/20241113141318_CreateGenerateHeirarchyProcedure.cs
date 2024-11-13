1cef41e8f058f1266f98e130a47865fa177bf6a8
Christo du Toit <christo@dutoit.co.uk>
2024-11-13 14:15:32 +0000

    DATA: GenerateHeirarchy migration

---
+++ ISL.ReIdentification.Core/Migrations/20241113141318_CreateGenerateHeirarchyProcedure.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.IO;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ISL.ReIdentification.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGenerateHeirarchyProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var resourceName =
+                $"ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications.Scripts.CreateGenerateHeirarchyProcedure.sql";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var sqlContent = reader.ReadToEnd();
+
+                var createSpIfNotExists = $@"
+                    IF NOT EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GenerateHeirarchy')
+                    BEGIN
+                        EXEC('{sqlContent.Replace("'", "''")}');
+                    END";
+
+                migrationBuilder.Sql(createSpIfNotExists);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                $@"IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'GenerateHeirarchy') 
+                BEGIN                
+                    DROP PROCEDURE GenerateHeirarchy;
+                END
+                ");
+        }
+    }
+}
