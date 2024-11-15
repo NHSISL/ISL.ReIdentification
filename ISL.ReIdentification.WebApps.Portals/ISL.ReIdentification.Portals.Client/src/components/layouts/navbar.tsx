2c4b12026384f6ec67fb2366637c444acd6b55bb
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-15 11:36:11 +0000

    CODE RUB: Login Page change

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/components/layouts/navbar.tsx
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/components/layouts/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars} from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Button, Container, Navbar } from "react-bootstrap";
 import Login from '../securitys/login';
 
@@ -22,8 +22,8 @@ const NavbarComponent: React.FC<NavbarComponentProps> = ({ toggleSidebar, showMe
                 <Navbar.Brand href="/" className="me-auto ms-3">
                     <img src="LHDLogoRound.png" alt="London Data Service logo" height="30" width="30" />
                     <span style={{ marginLeft: "10px" }}>
-                    London Data Service -
-                        <strong className="hero-text"> Re-Identification User Portal</strong>
+                        London Data Service -
+                        <strong className="hero-text"> Re-Identification</strong>
                     </span>
                 </Navbar.Brand>
                 <Navbar.Text>
