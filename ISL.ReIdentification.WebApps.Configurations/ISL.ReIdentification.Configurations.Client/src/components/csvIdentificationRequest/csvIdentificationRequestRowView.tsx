7df22e0397bf42dc52ab2379978a9692fa499455
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-18 12:28:24 +0000

    CODE RUB: Lint Issue

--- ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/components/csvIdentificationRequest/csvIdentificationRequestRowView.tsx
+++ ISL.ReIdentification.WebApps.Configurations/ISL.ReIdentification.Configurations.Client/src/components/csvIdentificationRequest/csvIdentificationRequestRowView.tsx
@@ -1,6 +1,4 @@
 import { FunctionComponent } from "react";
-import { Link } from 'react-router-dom';
-import { Button } from "react-bootstrap";
 import { CsvIdentificationRequest } from "../../models/csvIdentificationRequest/csvIdentificationRequest";
 
 type CsvIdentificationRequestRowProps = {
