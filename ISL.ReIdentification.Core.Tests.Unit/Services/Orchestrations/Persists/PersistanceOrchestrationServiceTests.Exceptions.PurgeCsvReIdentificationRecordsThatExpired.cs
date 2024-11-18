69edaba8ddbd4757a7e12b0b51e9555ff4ed2beb
Joe Parkes <joe.owens@answerdigital.com>
2024-11-18 09:53:22 +0000

    ShouldThrowServiceExceptionOnPurgeCsvIdentificationRecordsThatExpiredIfServiceErrorOccurredAndLogItAsync -> FAIL

--- ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Persists/PersistanceOrchestrationServiceTests.Exceptions.PurgeCsvReIdentificationRecordsThatExpired.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Orchestrations/Persists/PersistanceOrchestrationServiceTests.Exceptions.PurgeCsvReIdentificationRecordsThatExpired.cs
@@ -2,6 +2,7 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using ISL.ReIdentification.Core.Models.Orchestrations.Persists.Exceptions;
@@ -96,6 +97,59 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Orchestrations.Persists
             this.loggingBrokerMock.Verify(broker =>
                broker.LogErrorAsync(It.Is(SameExceptionAs(
                    expectedPersistanceOrchestrationDependencyException))),
+                       Times.Once);
+
+            this.csvIdentificationRequestServiceMock.VerifyNoOtherCalls();
+            this.impersonationContextServiceMock.VerifyNoOtherCalls();
+            this.notificationServiceMock.VerifyNoOtherCalls();
+            this.accessAuditServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.hashBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.identifierBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowServiceExceptionOnPurgeCsvIdentificationRecordsThatExpiredIfServiceErrorOccurredAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedServicePersistanceOrchestrationException =
+                new FailedServicePersistanceOrchestrationException(
+                    message: "Failed service persistance orchestration error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedPersistanceOrchestrationServiceException =
+                new PersistanceOrchestrationServiceException(
+                    message: "Persistance orchestration service error occurred, contact support.",
+                    innerException: failedServicePersistanceOrchestrationException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .Throws(serviceException);
+
+            // when
+            ValueTask purgeCsvIdentificationRequestAsyncTask =
+                this.persistanceOrchestrationService.PurgeCsvReIdentificationRecordsThatExpired();
+
+            PersistanceOrchestrationServiceException
+                actualPersistanceOrchestrationValidationException =
+                await Assert.ThrowsAsync<PersistanceOrchestrationServiceException>(
+                    testCode: purgeCsvIdentificationRequestAsyncTask.AsTask);
+
+            // then
+            actualPersistanceOrchestrationValidationException.Should().BeEquivalentTo(
+                expectedPersistanceOrchestrationServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogErrorAsync(It.Is(SameExceptionAs(
+                   expectedPersistanceOrchestrationServiceException))),
                        Times.Once);
 
             this.csvIdentificationRequestServiceMock.VerifyNoOtherCalls();
