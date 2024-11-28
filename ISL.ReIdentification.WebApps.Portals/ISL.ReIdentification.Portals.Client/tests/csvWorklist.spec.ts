d35fee5fd72481916ce989c53a7fe127f940b57a
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-28 15:57:17 +0000

    CODE RUB: Lint

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/csvWorklist.spec.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/csvWorklist.spec.ts
@@ -1,17 +1,6 @@
-import { test, expect, Page } from '@playwright/test';
-import testDataForDataset from './testData/testDataForDataset.json' assert { type: 'json' };
-
+import { test } from '@playwright/test';
 
 test.describe('Re-Identify dataset Patient Tests', () => {
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto('https://localhost:5173/home');
-    });
-
-    testDataForDataset.forEach(({ recipientEmail, reason }) => {
-        test(`Can Worklist: ${recipientEmail}`, async ({ page }) => {
-            
-        });
-    });
 
 });
