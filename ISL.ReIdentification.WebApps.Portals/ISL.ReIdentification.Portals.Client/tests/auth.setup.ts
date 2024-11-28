4fb68e1f78d91473a9f3a0dbc6f73303f52917ab
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-11-28 15:52:46 +0000

    CODE RUB: Remove

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/auth.setup.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/auth.setup.ts
@@ -13,7 +13,7 @@ setup('authenticate', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign in' }).click();
     await page.getByLabel('yourusername@nelcsu.nhs.uk').fill('david.hayes17@nelcsu.nhs.uk'); //username
   await page.getByRole('button', { name: 'Next' }).click();
-    await page.getByLabel('Password').fill('l!8yKt3nQOWCI1wY'); // password
+    await page.getByLabel('Password').fill(''); // password
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
   await page.waitForURL('https://localhost:5173/');
