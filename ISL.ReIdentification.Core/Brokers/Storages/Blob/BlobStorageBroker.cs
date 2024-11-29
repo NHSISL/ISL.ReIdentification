b4c577057846d88733b135c2f7203b5a3699420c
Joe Parkes <joe.owens@answerdigital.com>
2024-11-29 10:27:50 +0000

    CreateDirectorySasTokenAsync implementation

--- ISL.ReIdentification.Core/Brokers/Storages/Blob/BlobStorageBroker.cs
+++ ISL.ReIdentification.Core/Brokers/Storages/Blob/BlobStorageBroker.cs
@@ -77,5 +77,19 @@ namespace ISL.ReIdentification.Core.Brokers.Storages.Blob
         /// <returns>A <see cref="ValueTask{List{String}}"/> containing the list of file names.</returns>
         public async ValueTask<List<string>> ListFilesInContainerAsync(string container) =>
             await this.storageAbstractionProvider.ListFilesInContainerAsync(container);
+
+        /// <summary>
+        /// Creates a SAS token scoped to the provided container and directory, with the permissions of 
+        /// the provided access policy.
+        /// </summary>
+        /// <param name="container">The name of the storage container where the SAS token will be created.</param>
+        /// <param name="directoryPath">The path to which the SAS token will be scoped</param>
+        /// <param name="accessPolicyIdentifier">The name of the stored access policy.</param>
+        /// <param name="expiresOn">The <see cref="DateTimeOffset"/> indicating when the SAS token will expire.</param>
+        /// <returns>A <see cref="ValueTask{String}"/> containing the generated access token.</returns>
+        public async ValueTask<string> CreateDirectorySasTokenAsync(
+            string container, string directoryPath, string accessPolicyIdentifier, DateTimeOffset expiresOn) =>
+            await this.storageAbstractionProvider.CreateDirectorySasTokenAsync(
+                container, directoryPath, accessPolicyIdentifier, expiresOn);
     }
 }
