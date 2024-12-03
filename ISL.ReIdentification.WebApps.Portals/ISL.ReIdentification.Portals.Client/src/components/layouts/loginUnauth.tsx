a975dcab2d91a0531ad247ffa2a23a1a241bef29
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-12-03 10:13:33 +0000

    CODE RUB: Fix London Spelling

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/components/layouts/loginUnauth.tsx
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/components/layouts/loginUnauth.tsx
@@ -30,7 +30,7 @@ export const LoginUnAuthorisedComponent = () => {
 
                             </Card.Title>
                             <Card.Text className="mb-4 align-items-left" >
-                                <p>Welcome to the Lodon Data Service Reidentification Portal.</p>
+                                <p>Welcome to the London Data Service Reidentification Portal.</p>
                                 <p>Please sign in to continue.</p>
                             </Card.Text>
                             <Button onClick={handleLoginRedirect} className="me-3">Sign in</Button>
