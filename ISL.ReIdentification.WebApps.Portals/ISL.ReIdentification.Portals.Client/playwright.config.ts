cad7acb56dcf89d1b2ec040f3a5790272b799419
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-12-05 11:37:26 +0000

    CODE RUB: Lint

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/playwright.config.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/playwright.config.ts
@@ -9,8 +9,8 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 
 const authFile = path.join(__dirname, './playwright/.auth/user.json');
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });;
-dotenv.config({ path: path.resolve(__dirname, '.env.local') });;
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
 
 if (!process.env.TEST_USERNAME || !process.env.TEST_PASSWORD) {
     console.log("\x1b[31m");
@@ -29,7 +29,7 @@ if (!process.env.TEST_USERNAME || !process.env.TEST_PASSWORD) {
 }
 
 try {
-    atob(process.env.TEST_PASSWORD)
+    atob(process.env.TEST_PASSWORD);
 } catch (ex) {
     console.log("\x1b[31m");
     console.log("TEST_PASSWORD is not base64 encoded. Please encode the password with btoa() before setting the environment variable.");
@@ -42,68 +42,41 @@ try {
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    testDir: './tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
+    },
 
-  },
+    /* Configure projects for major browsers */
+    projects: [
+        { name: 'setup', testMatch: /.*\.setup\.ts/ },
+        {
+            name: 'Microsoft Edge',
+            use: { ...devices['Desktop Edge'], channel: 'msedge', storageState: authFile },
+            dependencies: ['setup']
+        },
+    ],
 
-  /* Configure projects for major browsers */
-  projects: [
-    { name: 'setup', testMatch: /.*\.setup\.ts/ },
-    //{
-    //  name: 'chromium',
-    //  use: { ...devices['Desktop Chrome'] },
-    //},
-    //{
-    //  name: 'firefox',
-    //  use: { ...devices['Desktop Firefox'] },
-    //},
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-     {
-       name: 'Microsoft Edge',
-       use: { ...devices['Desktop Edge'], channel: 'msedge', storageState: authFile, },
-       dependencies: ['setup']
-     },
-     //{
-     //  name: 'Google Chrome',
-     //  use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-     //},
-  ],
-
-  /* Run your local dev server before starting the tests */
-   webServer: {
-     command: "dotnet run --project ../ISL.ReIdentification.Portals.Server/",
-     url: 'https://localhost:5173/',
-     reuseExistingServer: true,
-     ignoreHTTPSErrors: true,
+    /* Run your local dev server before starting the tests */
+    webServer: {
+        command: "dotnet run --project ../ISL.ReIdentification.Portals.Server/",
+        url: 'https://localhost:5173/',
+        reuseExistingServer: true,
+        ignoreHTTPSErrors: true,
     },
 });
-
-
--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/navigation.spec.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/navigation.spec.ts
@@ -1,20 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://localhost:5173/home');
+
+test.describe('navigation', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://localhost:5173/home');
+    });
+
+    test('has title', async ({ page }) => {
+        await expect(page).toHaveTitle("Re-Identification Portal");
+    });
+
+    test('has navigation', async ({ page }) => {
+        await expect(page.getByRole('link', { name: 'ReIdentify Dataset' })).toBeVisible();
+        await expect(page.getByRole('link', { name: 'My Dataset Worklist' })).toBeVisible();
+        await expect(page.getByRole('link', { name: 'Re-identify Single Patient' })).toBeVisible();
+        await expect(page.getByRole('link', { name: 'Report Re-identification' })).toBeVisible();
+        await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible();
+    });
 });
-
-test('has title', async ({ page }) => {
-    await expect(page).toHaveTitle("Re-Identification Portal");
-});
-
-test('has navigation', async ({ page }) => {
-    await expect(page.getByRole('link', { name: 'ReIdentify Dataset' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'My Dataset Worklist' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Re-identify Single Patient' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Report Re-identification' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible();
-});
-
-
 
