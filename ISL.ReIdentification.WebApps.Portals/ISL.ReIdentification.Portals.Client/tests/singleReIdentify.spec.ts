74ae10f77d57b4b284916dbc3d6ac998c2a3e174
David Hayes <3583738+davidhayes03@users.noreply.github.com>
2024-12-05 09:36:13 +0000

    CODE RUB: Updates Describes

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/singleReIdentify.spec.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/tests/singleReIdentify.spec.ts
@@ -24,139 +24,136 @@ async function fillReIdentifyForm(page: Page, pseudoNumber: string, reason: stri
     await getNhsNumberButton.click();
 }
 
-
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://localhost:5173/home');
-});
-
-testData.forEach(({ validHexNumber, reason, expectedText }) => {
-    test(`has Access to Patient: ${validHexNumber}`, async ({ page }) => {
-        await navigateToReIdentifySinglePatient(page);
-        await fillReIdentifyForm(page, validHexNumber, reason);
-
-        const startOverButton = page.getByRole('button', { name: 'Start Over' });
-        await startOverButton.waitFor({ state: 'visible' })
-        await expect(startOverButton).toBeVisible({ timeout: 5000 });
-
-        const nhsNumberText = page.getByText(expectedText);
-        await nhsNumberText.waitFor({ state: 'visible' });
-        await expect(nhsNumberText).toBeVisible({ timeout: 5000 });
-
-        const hidingInText = page.getByText('Hiding in:');
-        await hidingInText.waitFor({ state: 'visible' });
-        await expect(hidingInText).toBeVisible({ timeout: 5000 });
-
-        await startOverButton.click();
-        const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
-        await expect(link).toBeVisible({ timeout: 5000 });
+test.describe('Single Re-Identify Tests', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://localhost:5173/home');
     });
-});
 
-testData.forEach(({ inValidHexNumber, reason, inValidExpectedText }) => {
-    test(`has No Access to Patient: ${inValidHexNumber}`, async ({ page }) => {
+    testData.forEach(({ validHexNumber, reason, expectedText }) => {
+        test(`has Access to Patient: ${validHexNumber}`, async ({ page }) => {
+            await navigateToReIdentifySinglePatient(page);
+            await fillReIdentifyForm(page, validHexNumber, reason);
+
+            const startOverButton = page.getByRole('button', { name: 'Start Over' });
+            await startOverButton.waitFor({ state: 'visible' });
+            await expect(startOverButton).toBeVisible({ timeout: 5000 });
+
+            const nhsNumberText = page.getByText(expectedText);
+            await nhsNumberText.waitFor({ state: 'visible' });
+            await expect(nhsNumberText).toBeVisible({ timeout: 5000 });
+
+            const hidingInText = page.getByText('Hiding in:');
+            await hidingInText.waitFor({ state: 'visible' });
+            await expect(hidingInText).toBeVisible({ timeout: 5000 });
+
+            await startOverButton.click();
+            const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
+            await expect(link).toBeVisible({ timeout: 5000 });
+        });
+    });
+
+    testData.forEach(({ inValidHexNumber, reason, inValidExpectedText }) => {
+        test(`has No Access to Patient: ${inValidHexNumber}`, async ({ page }) => {
+            await navigateToReIdentifySinglePatient(page);
+            await fillReIdentifyForm(page, inValidHexNumber, reason);
+
+            const startOverButton = page.getByRole('button', { name: 'Start Over' });
+            await startOverButton.waitFor({ state: 'visible' });
+            await expect(startOverButton).toBeVisible({ timeout: 5000 });
+
+            const noAccessText = page.getByText(inValidExpectedText);
+            await noAccessText.waitFor({ state: 'visible', timeout: 20000 });
+            await expect(noAccessText).toBeVisible({ timeout: 5000 });
+
+            await startOverButton.click();
+            const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
+            await expect(link).toBeVisible({ timeout: 5000 });
+        });
+    });
+
+    test('verifies No Access to Non-NHS Number', async ({ page }) => {
         await navigateToReIdentifySinglePatient(page);
-        await fillReIdentifyForm(page, inValidHexNumber, reason);
+        await fillReIdentifyForm(page, '111111XXXX', 'Direct patient care');
 
         const startOverButton = page.getByRole('button', { name: 'Start Over' });
-        await startOverButton.waitFor({ state: 'visible' });
         await expect(startOverButton).toBeVisible({ timeout: 5000 });
 
-        const noAccessText = page.getByText(inValidExpectedText);
-        await noAccessText.waitFor({ state: 'visible', timeout: 20000 })
+        const noAccessText = page.getByText('It appears you tried to re-identify a patient you don\'t have access to.Please');
         await expect(noAccessText).toBeVisible({ timeout: 5000 });
 
         await startOverButton.click();
         const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
         await expect(link).toBeVisible({ timeout: 5000 });
-
     });
-});
 
-test('verifies No Access to Non-NHS Number', async ({ page }) => {
-    await navigateToReIdentifySinglePatient(page);
-    await fillReIdentifyForm(page, '111111XXXX', 'Direct patient care');
-
-    const startOverButton = page.getByRole('button', { name: 'Start Over' });
-    await expect(startOverButton).toBeVisible({ timeout: 5000 });
-
-    const noAccessText = page.getByText('It appears you tried to re-identify a patient you don\'t have access to.Please');
-    await expect(noAccessText).toBeVisible({ timeout: 5000 });
-
-    await startOverButton.click();
-    const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
-    await expect(link).toBeVisible({ timeout: 5000 });
-
-});
-
-test('verifies No Access to Non-NHS Length Number', async ({ page }) => {
-    await navigateToReIdentifySinglePatient(page);
-    await fillReIdentifyForm(page, '11111', 'Direct patient care');
-
-    //No popup to say why??
-    // fill out all and make sure button isnt active
-});
-
-testData.forEach(({ validHexNumber, reason, expectedText }) => {
-    test(`Has Access - to valid Hex Numbers: ${validHexNumber}`, async ({ page }) => {
+    test('verifies No Access to Non-NHS Length Number', async ({ page }) => {
         await navigateToReIdentifySinglePatient(page);
-        await fillReIdentifyForm(page, validHexNumber, reason);
+        await fillReIdentifyForm(page, '11111', 'Direct patient care');
 
-        const startOverButton = page.getByRole('button', { name: 'Start Over' });
-        await startOverButton.waitFor({ state: 'visible', timeout: 10000 })
-        await expect(startOverButton).toBeVisible({ timeout: 5000 });
-
-        const nhsNumberText = page.getByText(expectedText);
-        await nhsNumberText.waitFor({ state: 'visible' });
-        await expect(nhsNumberText).toBeVisible({ timeout: 5000 });
-
-        const hidingInText = page.getByText('Hiding in:');
-        await hidingInText.waitFor({ state: 'visible' });
-        await expect(hidingInText).toBeVisible({ timeout: 5000 });
-
-        await startOverButton.click();
-        const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
-        await expect(link).toBeVisible({ timeout: 5000 });
-
+        // No popup to say why??
+        // fill out all and make sure button isn't active
     });
-});
 
-testData.forEach(({ inValidHexNumber, reason, inValidExpectedText }) => {
-    test(`verifies No Access to Non-NHS Hex Number Number: ${inValidHexNumber}`, async ({ page }) => {
-        await navigateToReIdentifySinglePatient(page);
-        await fillReIdentifyForm(page, inValidHexNumber, reason);
+    testData.forEach(({ validHexNumber, reason, expectedText }) => {
+        test(`Has Access - to valid Hex Numbers: ${validHexNumber}`, async ({ page }) => {
+            await navigateToReIdentifySinglePatient(page);
+            await fillReIdentifyForm(page, validHexNumber, reason);
 
-        const startOverButton = page.getByRole('button', { name: 'Start Over' });
-        await startOverButton.waitFor({ state: 'visible' });
-        await expect(startOverButton).toBeVisible({ timeout: 5000 });
+            const startOverButton = page.getByRole('button', { name: 'Start Over' });
+            await startOverButton.waitFor({ state: 'visible', timeout: 10000 });
+            await expect(startOverButton).toBeVisible({ timeout: 5000 });
 
-        const noAccessText = page.getByText(inValidExpectedText);
-        await noAccessText.waitFor({ state: 'visible', timeout: 20000 })
-        await expect(noAccessText).toBeVisible({ timeout: 5000 });
+            const nhsNumberText = page.getByText(expectedText);
+            await nhsNumberText.waitFor({ state: 'visible' });
+            await expect(nhsNumberText).toBeVisible({ timeout: 5000 });
 
-        await startOverButton.click();
-        const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
-        await expect(link).toBeVisible();
+            const hidingInText = page.getByText('Hiding in:');
+            await hidingInText.waitFor({ state: 'visible' });
+            await expect(hidingInText).toBeVisible({ timeout: 5000 });
 
+            await startOverButton.click();
+            const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
+            await expect(link).toBeVisible({ timeout: 5000 });
+        });
     });
-});
 
-testData.forEach(({ validPseudoNumber, reason, expectedText }) => {
-    test(`Has Access -  Re-identify with pseudoNumber: ${validPseudoNumber}`, async ({ page }) => {
-        const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
-        await expect(link).toBeVisible();
-        await link.click();
+    testData.forEach(({ inValidHexNumber, reason, inValidExpectedText }) => {
+        test(`verifies No Access to Non-NHS Hex Number Number: ${inValidHexNumber}`, async ({ page }) => {
+            await navigateToReIdentifySinglePatient(page);
+            await fillReIdentifyForm(page, inValidHexNumber, reason);
 
-        const pseudoNHSNumberInput = page.getByPlaceholder('Pseudo Number');
-        await pseudoNHSNumberInput.fill(validPseudoNumber);
+            const startOverButton = page.getByRole('button', { name: 'Start Over' });
+            await startOverButton.waitFor({ state: 'visible' });
+            await expect(startOverButton).toBeVisible({ timeout: 5000 });
 
-        const reasonDropdown = page.getByRole('combobox');
-        await reasonDropdown.selectOption({ label: reason });
+            const noAccessText = page.getByText(inValidExpectedText);
+            await noAccessText.waitFor({ state: 'visible', timeout: 20000 });
+            await expect(noAccessText).toBeVisible({ timeout: 5000 });
 
-        const getNhsNumberButton = page.getByRole('button', { name: 'Get Nhs Number' });
-        await getNhsNumberButton.click();
+            await startOverButton.click();
+            const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
+            await expect(link).toBeVisible();
+        });
+    });
 
-        // Assert the expected output
-        page.getByText(expectedText);
+    testData.forEach(({ validPseudoNumber, reason, expectedText }) => {
+        test(`Has Access -  Re-identify with pseudoNumber: ${validPseudoNumber}`, async ({ page }) => {
+            const link = page.getByRole('link', { name: 'Re-identify Single Patient' });
+            await expect(link).toBeVisible();
+            await link.click();
 
+            const pseudoNHSNumberInput = page.getByPlaceholder('Pseudo Number');
+            await pseudoNHSNumberInput.fill(validPseudoNumber);
+
+            const reasonDropdown = page.getByRole('combobox');
+            await reasonDropdown.selectOption({ label: reason });
+
+            const getNhsNumberButton = page.getByRole('button', { name: 'Get Nhs Number' });
+            await getNhsNumberButton.click();
+
+            // Assert the expected output
+            const nhsNumberText = page.getByText(expectedText);
+            await expect(nhsNumberText).toBeVisible();
+        });
     });
 });
