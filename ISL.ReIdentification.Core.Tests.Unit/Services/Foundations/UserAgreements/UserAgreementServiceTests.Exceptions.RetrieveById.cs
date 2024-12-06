ac2c236bb3b68c64afa476868af5b01985ff5cd1
Christo du Toit <christo@dutoit.co.uk>
2024-12-06 14:11:56 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/UserAgreements/UserAgreementServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using ISL.ReIdentification.Core.Models.Foundations.UserAgreements;
+using ISL.ReIdentification.Core.Models.Foundations.UserAgreements.Exceptions;
+using Xunit;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.UserAgreements
+{
+    public partial class UserAgreementServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            SqlException sqlException = GetSqlException();
+
+            var failedUserAgreementStorageException =
+                new FailedUserAgreementStorageException(
+                    message: "Failed userAgreement storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedUserAgreementDependencyException =
+                new UserAgreementDependencyException(
+                    message: "UserAgreement dependency error occurred, contact support.",
+                    innerException: failedUserAgreementStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserAgreementByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<UserAgreement> retrieveUserAgreementByIdTask =
+                this.userAgreementService.RetrieveUserAgreementByIdAsync(someId);
+
+            UserAgreementDependencyException actualUserAgreementDependencyException =
+                await Assert.ThrowsAsync<UserAgreementDependencyException>(
+                    retrieveUserAgreementByIdTask.AsTask);
+
+            // then
+            actualUserAgreementDependencyException.Should()
+                .BeEquivalentTo(expectedUserAgreementDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserAgreementByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserAgreementDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
