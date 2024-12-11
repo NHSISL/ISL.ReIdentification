fc1f81333e36e8a259df1ebb9381f0daf48c5e14
MarcusElcamel <marcus.elcamel@answerdigital.com>
2024-12-11 11:25:37 +0000

    ShouldThrowDependencyValidationExceptionOnCreateDirectorySasTokenAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Documents/DocumentsTests.Exceptions.CreateDirectorySasToken.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.ReIdentification.Core.Models.Foundations.Documents.Exceptions;
+using Moq;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Documents
+{
+    public partial class DocumentsTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnCreateDirectorySasTokenAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string someContainer = GetRandomString();
+            string someDirectoryPath = GetRandomString();
+            string someAccessPolicyIdentifier = GetRandomString();
+            DateTimeOffset someDateTimeOffset = GetRandomFutureDateTimeOffset();
+
+            var expectedDependencyValidationException = new DocumentDependencyValidationException(
+                message: "Document dependency validation error occurred, please fix errors and try again.",
+                innerException: dependencyValidationException);
+
+            this.blobStorageBrokerMock.Setup(broker =>
+                broker.CreateDirectorySasTokenAsync(
+                    someContainer, someDirectoryPath, someAccessPolicyIdentifier, someDateTimeOffset))
+                        .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask<string> createDirectorySasTask =
+                this.documentService.CreateDirectorySasTokenAsync(
+                    someContainer, someDirectoryPath, someAccessPolicyIdentifier, someDateTimeOffset);
+
+            DocumentDependencyValidationException actualDocumentServiceException =
+                await Assert.ThrowsAsync<DocumentDependencyValidationException>(createDirectorySasTask.AsTask);
+
+            // then
+            actualDocumentServiceException.Should().BeEquivalentTo(expectedDependencyValidationException);
+
+            this.blobStorageBrokerMock.Verify(broker =>
+                broker.CreateDirectorySasTokenAsync(
+                    someContainer, someDirectoryPath, someAccessPolicyIdentifier, someDateTimeOffset),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedDependencyValidationException))),
+                    Times.Once);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
