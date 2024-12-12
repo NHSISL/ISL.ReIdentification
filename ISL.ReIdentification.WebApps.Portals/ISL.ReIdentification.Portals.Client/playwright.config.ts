abeb79718a52edf9fa6895aa366c5b8a3c79a7a0
David Cunliffe <david.cunliffe2@nhs.net>
2024-12-12 15:24:22 +0000

    coderub: playwright tests

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/playwright.config.ts
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/playwright.config.ts
@@ -91,10 +91,10 @@ export default defineConfig({
             use: { ...devices['Desktop Edge'], channel: 'msedge', storageState: authFile, },
             dependencies: ['setup']
         },
-        {
+       /* {
           name: 'Google Chrome',
           use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        },
+        },*/
     ],
 
     /* Run your local dev server before starting the tests */
