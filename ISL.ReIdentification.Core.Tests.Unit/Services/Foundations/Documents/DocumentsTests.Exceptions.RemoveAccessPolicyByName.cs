feb6dbe31e6d4e47f7fe0b5bec0b93a25efe3755
MarcusElcamel <marcus.elcamel@answerdigital.com>
2024-12-12 13:52:25 +0000

    ShouldThrowDependencyValidationExceptionOnRemoveAccessPolicyByNameAsync -> PASS

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Documents/DocumentsTests.Exceptions.RemoveAccessPolicyByName.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models.Exceptions;
+using ISL.ReIdentification.Core.Models.Foundations.Documents.Exceptions;
+using Moq;
+using Xeptions;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Documents
+{
+    public partial class DocumentsTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveAccessPolicyByNameAsync(
+            Xeption dependencyValidationException)
+        {
+            // given
+            string someContainer = GetRandomString();
+            string somPolicyName = GetRandomString();
+                
+            var expectedDocumentServiceException = new DocumentDependencyValidationException(
+                message: "Document dependency validation error occurred, please fix errors and try again.",
+                innerException: dependencyValidationException);
+
+            this.blobStorageBrokerMock.Setup(broker =>
+                broker.RemoveAccessPolicyByNameAsync(someContainer, somPolicyName))
+                    .ThrowsAsync(dependencyValidationException);
+
+            // when
+            ValueTask removeAccessPolicyByNameTask =
+                this.documentService.RemoveAccessPolicyByNameAsync(someContainer, somPolicyName);
+
+            DocumentDependencyValidationException actualDocumentServiceException =
+                await Assert.ThrowsAsync<DocumentDependencyValidationException>(removeAccessPolicyByNameTask.AsTask);
+
+            // then
+            actualDocumentServiceException.Should().BeEquivalentTo(expectedDocumentServiceException);
+
+            this.blobStorageBrokerMock.Verify(broker =>
+                broker.RemoveAccessPolicyByNameAsync(someContainer, somPolicyName),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedDocumentServiceException))),
+                        Times.Once);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
