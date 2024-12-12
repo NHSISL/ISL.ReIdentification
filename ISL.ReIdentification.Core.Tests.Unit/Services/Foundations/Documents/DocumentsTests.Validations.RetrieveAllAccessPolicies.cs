924ed5bbd79318122a134da632c922533e667d70
MarcusElcamel <marcus.elcamel@answerdigital.com>
2024-12-12 16:14:50 +0000

    ShouldThrowValidationExceptionOnRetrieveAllAccessPoliciesAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Documents/DocumentsTests.Validations.RetrieveAllAccessPolicies.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Providers.Storages.Abstractions.Models;
+using ISL.ReIdentification.Core.Models.Foundations.Documents.Exceptions;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Documents
+{
+    public partial class DocumentsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnRetrieveAllAccessPoliciesAsync(
+            string invalidString)
+        {
+            // given
+            string invalidContainer = invalidString;
+
+            var invalidDocumentException = new InvalidDocumentException(
+                message: "Invalid document. Please correct the errors and try again.");
+
+            invalidDocumentException.AddData(
+                key: "Container",
+                values: "Text is invalid");
+
+            var expectedDocumentValidationException = new DocumentValidationException(
+                message: "Document validation error occurred, please fix errors and try again.",
+                innerException: invalidDocumentException);
+
+            // when
+            ValueTask<List<Policy>> retrievedPoliciesTask =
+                this.documentService.RetrieveAllAccessPoliciesAsync(invalidContainer);
+
+            DocumentValidationException actualDocumentValidationException =
+                await Assert.ThrowsAsync<DocumentValidationException>(retrievedPoliciesTask.AsTask);
+
+            // then
+            actualDocumentValidationException.Should().BeEquivalentTo(expectedDocumentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedDocumentValidationException))),
+                        Times.Once);
+
+            this.blobStorageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
