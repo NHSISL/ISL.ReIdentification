91d1d9ea6c32cca1ad257072ded0f93e3c83571b
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-12-23 15:51:01 +0000

    ShouldExpireRenewImpersonationContextTokensAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Coordinations/Identifications/IdentificationCoordinationTests.Logic.ExpireRenewImpersonationContextTokens.cs
@@ -0,0 +1,92 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Coordinations.Identifications
+{
+    public partial class IdentificationCoordinationTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task ShouldExpireRenewImpersonationContextTokensAsync(bool isPreviosulyApproved)
+        {
+            // given
+            AccessRequest randomAccessRequest = CreateRandomAccessRequest();
+            randomAccessRequest.IdentificationRequest = null;
+            randomAccessRequest.CsvIdentificationRequest = null;
+            Guid inputImpersonationContextId = randomAccessRequest.ImpersonationContext.Id;
+            AccessRequest retrievedAccessRequest = randomAccessRequest.DeepClone();
+            retrievedAccessRequest.ImpersonationContext.IsApproved = isPreviosulyApproved;
+
+
+            AccessRequest approvedAccessRequest = retrievedAccessRequest.DeepClone();
+
+            if (!isPreviosulyApproved)
+            {
+                approvedAccessRequest.ImpersonationContext.IsApproved = true;
+            }
+
+            AccessRequest inputAccessRequest = approvedAccessRequest.DeepClone();
+            AccessRequest outputAccessRequest = inputAccessRequest.DeepClone();
+            AccessRequest expectedAccessRequest = outputAccessRequest.DeepClone();
+
+            this.persistanceOrchestrationServiceMock.Setup(service =>
+                service.RetrieveImpersonationContextByIdAsync(inputImpersonationContextId))
+                    .ReturnsAsync(retrievedAccessRequest);
+
+            if (!isPreviosulyApproved)
+            {
+                this.persistanceOrchestrationServiceMock.Setup(service =>
+                    service.PersistImpersonationContextAsync(It.Is(SameAccessRequestAs(approvedAccessRequest))))
+                        .ReturnsAsync(retrievedAccessRequest);
+            }
+
+            this.identificationOrchestrationServiceMock.Setup(service =>
+                service.ExpireRenewImpersonationContextTokensAsync(
+                    It.Is(SameAccessRequestAs(inputAccessRequest)),
+                    isPreviosulyApproved))
+                        .ReturnsAsync(outputAccessRequest);
+
+            // when
+            AccessRequest actualAccessRequest = await this.identificationCoordinationService
+                .ExpireRenewImpersonationContextTokensAsync(inputImpersonationContextId);
+
+            // then
+            actualAccessRequest.Should().BeEquivalentTo(expectedAccessRequest);
+
+            this.persistanceOrchestrationServiceMock.Verify(service =>
+                service.RetrieveImpersonationContextByIdAsync(inputImpersonationContextId),
+                    Times.Once);
+
+            if (!isPreviosulyApproved)
+            {
+                this.persistanceOrchestrationServiceMock.Verify(service =>
+                   service.PersistImpersonationContextAsync(It.Is(SameAccessRequestAs(approvedAccessRequest))),
+                        Times.Once);
+            }
+
+            this.identificationOrchestrationServiceMock.Verify(service =>
+                service.ExpireRenewImpersonationContextTokensAsync(
+                    It.Is(SameAccessRequestAs(inputAccessRequest)),
+                    isPreviosulyApproved),
+                        Times.Once);
+
+            this.persistanceOrchestrationServiceMock.Verify(service =>
+                service.SendGeneratedTokensNotificationAsync(It.Is(SameAccessRequestAs(outputAccessRequest))),
+                    Times.Once);
+
+            this.persistanceOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.accessOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.identificationOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
