18fc3e4289b6af0ee2b729bbd9bc97d6f1f1e1bb
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-03 11:58:44 +0000

    ShouldThrowValidationExceptionOnSendImpersonationTokensGeneratedNotificationIfObjectIsInvalidAndLogItAsync -> PASS

--- ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Notifications/NotificationTests.Validations.SendImpersonationTokensGeneratedNotification.cs
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Notifications/NotificationTests.Validations.SendImpersonationTokensGeneratedNotification.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using ISL.ReIdentification.Core.Models.Brokers.Notifications;
+using ISL.ReIdentification.Core.Models.Foundations.ImpersonationContexts;
 using ISL.ReIdentification.Core.Models.Foundations.Notifications.Exceptions;
 using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
 using ISL.ReIdentification.Core.Services.Foundations.Notifications;
@@ -34,6 +35,78 @@ namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Notification
             invalidArgumentsNotificationException.AddData(
                 key: nameof(NotificationConfigurations),
                 values: $"{nameof(NotificationConfigurations)} is invalid");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation error occurred, please fix errors and try again.",
+                    innerException: invalidArgumentsNotificationException);
+
+            NotificationService notificationService =
+                new NotificationService(
+                    notificationConfigurations: invalidNotificationConfigurations,
+                    notificationBroker: this.notificationBrokerMock.Object,
+                    loggingBroker: this.loggingBrokerMock.Object);
+
+            // when
+            ValueTask sendImpersonationTokensGeneratedNotificationTask =
+                notificationService.SendImpersonationTokensGeneratedNotificationAsync(invalidAccessRequest);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(
+                    testCode: sendImpersonationTokensGeneratedNotificationTask.AsTask);
+
+            // then
+            actualNotificationValidationException.Should()
+                .BeEquivalentTo(expectedNotificationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNotificationValidationException))),
+                        Times.Once);
+
+            this.notificationBrokerMock.Verify(broker =>
+                broker.SendEmailAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, dynamic>>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.notificationBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnSendImpersonationTokensGeneratedNotificationIfObjectIsInvalidAndLogItAsync()
+        {
+            // given
+            AccessRequest invalidAccessRequest = new AccessRequest();
+            NotificationConfigurations invalidNotificationConfigurations = new NotificationConfigurations();
+
+            var invalidArgumentsNotificationException =
+                new InvalidArgumentsNotificationException(
+                    message: "Invalid notification arguments. Please correct the errors and try again.");
+
+            invalidArgumentsNotificationException.AddData(
+                key: $"{nameof(AccessRequest)}." +
+                    $"{nameof(AccessRequest.ImpersonationContext)}",
+                values: $"{nameof(ImpersonationContext)} is invalid");
+
+            invalidArgumentsNotificationException.AddData(
+                key: $"{nameof(NotificationConfigurations)}." +
+                    $"{nameof(NotificationConfigurations.ImpersonationTokensGeneratedTemplateId)}",
+                values: "Text is invalid");
+
+            invalidArgumentsNotificationException.AddData(
+                key: $"{nameof(NotificationConfigurations)}." +
+                    $"{nameof(NotificationConfigurations.ConfigurationBaseUrl)}",
+                values: "Text is invalid");
+
+            invalidArgumentsNotificationException.AddData(
+                key: $"{nameof(NotificationConfigurations)}." +
+                    $"{nameof(NotificationConfigurations.PortalBaseUrl)}",
+                values: "Text is invalid");
 
             var expectedNotificationValidationException =
                 new NotificationValidationException(
