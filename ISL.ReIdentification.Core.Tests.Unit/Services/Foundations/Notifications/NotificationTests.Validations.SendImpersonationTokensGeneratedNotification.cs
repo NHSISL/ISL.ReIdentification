e253ba2c6bd289c7edc38ed40103e2c77bc8a65a
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-01-03 11:49:43 +0000

    ShouldThrowValidationExceptionOnSendImpersonationTokensGeneratedNotificationIfArgInvalidAndLogItAsync -> FAIL

---
+++ ISL.ReIdentification.Core.Tests.Unit/Services/Foundations/Notifications/NotificationTests.Validations.SendImpersonationTokensGeneratedNotification.cs
@@ -0,0 +1,78 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.ReIdentification.Core.Models.Brokers.Notifications;
+using ISL.ReIdentification.Core.Models.Foundations.Notifications.Exceptions;
+using ISL.ReIdentification.Core.Models.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Services.Foundations.Notifications;
+using Moq;
+
+namespace ISL.ReIdentification.Core.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnSendImpersonationTokensGeneratedNotificationIfArgInvalidAndLogItAsync()
+        {
+            // given
+            AccessRequest invalidAccessRequest = null;
+            NotificationConfigurations invalidNotificationConfigurations = null;
+
+            var invalidArgumentsNotificationException =
+                new InvalidArgumentsNotificationException(
+                    message: "Invalid notification arguments. Please correct the errors and try again.");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(AccessRequest),
+                values: $"{nameof(AccessRequest)} is invalid");
+
+            invalidArgumentsNotificationException.AddData(
+                key: nameof(NotificationConfigurations),
+                values: $"{nameof(NotificationConfigurations)} is invalid");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation error occurred, please fix errors and try again.",
+                    innerException: invalidArgumentsNotificationException);
+
+            NotificationService notificationService =
+                new NotificationService(
+                    notificationConfigurations: invalidNotificationConfigurations,
+                    notificationBroker: this.notificationBrokerMock.Object,
+                    loggingBroker: this.loggingBrokerMock.Object);
+
+            // when
+            ValueTask sendImpersonationTokensGeneratedNotificationTask =
+                notificationService.SendImpersonationTokensGeneratedNotificationAsync(invalidAccessRequest);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(
+                    testCode: sendImpersonationTokensGeneratedNotificationTask.AsTask);
+
+            // then
+            actualNotificationValidationException.Should()
+                .BeEquivalentTo(expectedNotificationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedNotificationValidationException))),
+                        Times.Once);
+
+            this.notificationBrokerMock.Verify(broker =>
+                broker.SendEmailAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, dynamic>>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.notificationBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
