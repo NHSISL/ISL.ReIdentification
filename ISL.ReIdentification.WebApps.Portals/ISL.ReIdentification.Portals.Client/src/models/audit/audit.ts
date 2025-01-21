c96b016ccc44416fe48e0e9415030a0f30750e4e
davidhayes03 <david.hayes17@nhs.net>
2025-01-21 09:34:36 +0000

    MODEL: audit model

---
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Client/src/models/audit/audit.ts
@@ -0,0 +1,11 @@
+export class audit {
+    public id: string = "";
+    public correlationId: string = "";
+    public auditType: string = "";
+    public auditDetail: string = "";
+    public logLevel: string = "";
+    public createdBy?: string;
+    public createdDate?: Date | undefined;
+    public updatedBy?: string;
+    public updatedDate?: Date | undefined;
+}
