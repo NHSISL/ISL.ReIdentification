5353d8cee0363de4a7dec070afc9ea8a6c3123a8
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-02-20 16:03:25 +0000

    ShouldReturnInternalServerErrorPostImpersonationContextApprovaIfServerErrorOccurredAsync -> PASS

--- ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Server.Tests.Unit/Controllers/ReIdentification/ReIdentificationControllerTests.Exceptions.PostImpersonationContextApproval.cs
+++ ISL.ReIdentification.WebApps.Portals/ISL.ReIdentification.Portals.Server.Tests.Unit/Controllers/ReIdentification/ReIdentificationControllerTests.Exceptions.PostImpersonationContextApproval.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RESTFulSense.Models;
 using Xeptions;
 
 namespace ISL.ReIdentification.Portals.Server.Tests.Unit.Controllers.ReIdentification
@@ -27,6 +28,37 @@ namespace ISL.ReIdentification.Portals.Server.Tests.Unit.Controllers.ReIdentific
             this.identificationCoordinationServiceMock.Setup(service =>
                 service.ImpersonationContextApprovalAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult actualActionResult =
+                await this.reIdentificationController.PostImpersonationContextApprovalAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<bool>());
+
+            // then
+            actualActionResult.Should().BeEquivalentTo(expectedActionResult);
+
+            this.identificationCoordinationServiceMock.Verify(service =>
+                service.ImpersonationContextApprovalAsync(It.IsAny<Guid>(), It.IsAny<bool>()),
+                    Times.Once);
+
+            this.identificationCoordinationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorPostImpersonationContextApprovaIfServerErrorOccurredAsync(
+            Xeption serverException)
+        {
+            // given
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult = expectedInternalServerErrorObjectResult;
+
+            this.identificationCoordinationServiceMock.Setup(service =>
+                service.ImpersonationContextApprovalAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+                    .ThrowsAsync(serverException);
 
             // when
             ActionResult actualActionResult =
