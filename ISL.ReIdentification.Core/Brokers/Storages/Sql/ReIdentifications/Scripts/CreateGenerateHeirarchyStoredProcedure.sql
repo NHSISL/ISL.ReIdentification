df3d2d4d29e096a79f808d1a3fcf8e8a05efba5e
David Cunliffe <david.l.cunliffe@gmail.com>
2025-02-26 16:55:14 +0000

    BACKEND: Updating Load Scripts

--- ISL.ReIdentification.Core/Brokers/Storages/Sql/ReIdentifications/Scripts/CreateGenerateHeirarchyStoredProcedure.sql
+++ ISL.ReIdentification.Core/Brokers/Storages/Sql/ReIdentifications/Scripts/CreateGenerateHeirarchyStoredProcedure.sql
@@ -1,102 +1,301 @@
-﻿CREATE PROCEDURE [dbo].[GenerateHeirarchy]
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[GenerateHeirarchy]
 AS
 BEGIN
-    -- Check if the temporary table exists
-    IF OBJECT_ID('[dbo].[TempOdsInbound]') IS NULL
-    BEGIN
-        THROW 50000, 'Temp Table does not exist.', 1;
-    END
+    DECLARE @CorrelationId UNIQUEIDENTIFIER = NEWID();
+	BEGIN TRY
+        -- Check if the inbound tables exists
+        IF OBJECT_ID('Dictionary.OrganisationDescendent') IS NULL
+        BEGIN
+            THROW 50000, 'Inbound Dictionary.OrganisationDescendent Table Missing.', 1;
+        END
 
-    -- Check if the temporary table has data
-    IF (SELECT COUNT(*) FROM [dbo].[TempOdsInbound]) = 0
-    BEGIN
-        RAISERROR(N'Temp Table is empty.', 16, 1);
-        RETURN;
-    END
+        IF OBJECT_ID('Dictionary.Organisation') IS NULL
+        BEGIN
+            THROW 50000, 'Inbound Dictionary.Organisation Table Missing.', 1;
+        END
 
-    -- Begin transaction for data insertion and updates
-    BEGIN TRANSACTION;
+        -- Check if the inbound table has data
+        IF (SELECT COUNT(*) FROM Dictionary.OrganisationDescendent) = 0
+        BEGIN
+            THROW 50000, N'Inbound Dictionary.OrganisationDescendent Table is empty.', 1;
+        END
 
-    -- Clear existing data in OdsDatas table
-    TRUNCATE TABLE dbo.OdsDatas;
+        IF (SELECT COUNT(*) FROM Dictionary.Organisation) = 0
+        BEGIN
+            RAISERROR(N'Inbound Dictionary.Organisation Table is empty.', 16, 1);
+            RETURN;
+        END
 
-    -- Set active status for valid records
-    UPDATE [dbo].[TempOdsInbound]
-    SET IsActive = 1
-    WHERE PathStartDate <= GETDATE()
-      AND PathEndDate >= GETDATE()
-      AND RelationshipStartDate <= GETDATE()
-      AND RelationshipEndDate >= GETDATE();
+        RAISERROR('Load Commenced',0,1) WITH NOWAIT;
+        
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Load Commenced', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
 
-    -- Attach any 'SELF' relationships to the root node
-    UPDATE [dbo].[TempOdsInbound]
-    SET ParentId = 1, ParentPath = 'ROOT'
-    WHERE RelationshipType = 'SELF';
+        RAISERROR('Create Temp Table',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Create Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
 
-    -- Generate readable parent path and assign a parent ID
-    UPDATE [dbo].[TempOdsInbound]
-    SET ParentPath = LEFT(Path, LEN(Path) - LEN(OrganisationCode_Child) - LEN(RelationshipType) - 10)
-    WHERE RelationshipType <> 'SELF';
+        -- Clear existing data in OdsDatas table
+        CREATE TABLE [dbo].[TempOdsInbound](
+            Id int IDENTITY(1,1) NOT NULL,
+            [ParentPath] [varchar](500) NULL,
+            [ParentId] int NULL,
+            RowNumber int NULL,
+            OdsHierarchy hierarchyid NULL,
+            [SK_OrganisationID_Root] [int]  NULL,
+            [OrganisationCode_Root] [varchar](15)  NULL,
+            [OrganisationPrimaryRole_Root] [varchar](5) NULL,
+            [SK_OrganisationID_Parent] [int]  NULL,
+            [OrganisationCode_Parent] [varchar](15)  NULL,
+            [OrganisationPrimaryRole_Parent] [varchar](5) NULL,
+            [SK_OrganisationID_Child] [int]  NULL,
+            [OrganisationCode_Child] [varchar](15)  NULL,
+            [OrganisationPrimaryRole_Child] [varchar](5) NULL,
+            [RelationshipType] [varchar](5)  NULL,
+            [RelationshipStartDate] [date]  NULL,
+            [RelationshipEndDate] [date]  NULL,
+            [Path] [varchar](500)  NULL,
+            [Depth] [tinyint]  NULL,
+            [PathStartDate] [date]  NULL,
+            [PathEndDate] [date]  NULL,
+            [DateAdded] [date]  NULL,
+            [DateUpdated] [date]  NULL,
+            [ODSOrgName] [varchar](255) NULL,
+            [IsActive] [bit] NULL Default(1),
+        ) ON [PRIMARY]
 
-    -- Assign ParentId using derived parent path
-    UPDATE od
-    SET ParentId = (
-        SELECT id 
-        FROM [dbo].[TempOdsInbound] od1 
-        WHERE od1.path = od.ParentPath
-    )
-    FROM [dbo].[TempOdsInbound] od
-    WHERE ParentId IS NULL;
+        INSERT INTO TempOdsInbound (OrganisationCode_Child, IsActive) VALUES ('ROOT',1)
 
-    -- Assign row numbers for each child
-    UPDATE od
-    SET RowNumber = rn
-    FROM [dbo].[TempOdsInbound] od
-    JOIN (
-        SELECT id, ROW_NUMBER() OVER (PARTITION BY ParentId ORDER BY ParentId) AS rn 
-        FROM [dbo].[TempOdsInbound]
-    ) od1 ON od.id = od1.id;
+        INSERT INTO TempOdsInbound 
+        (
+            [SK_OrganisationID_Root]        ,
+            [OrganisationCode_Root]         ,
+            [OrganisationPrimaryRole_Root]  ,
+            [SK_OrganisationID_Parent]      ,
+            [OrganisationCode_Parent]       ,
+            [OrganisationPrimaryRole_Parent],
+            [SK_OrganisationID_Child]       ,
+            [OrganisationCode_Child]        ,
+            [OrganisationPrimaryRole_Child] ,
+            [RelationshipType]              ,
+            [RelationshipStartDate]         ,
+            [RelationshipEndDate]           ,
+            [Path]                          ,
+            [Depth]                         ,
+            [PathStartDate]                 ,
+            [PathEndDate]                   ,
+            [DateAdded]                     ,
+            [DateUpdated]                   ,
+            [ODSOrgName]                    ,
+            [IsActive]              
+        )
+        SELECT      
+            [SK_OrganisationID_Root]        ,
+            [OrganisationCode_Root]         ,
+            [OrganisationPrimaryRole_Root]  ,
+            [SK_OrganisationID_Parent]      ,
+            [OrganisationCode_Parent]       ,
+            [OrganisationPrimaryRole_Parent],
+            [SK_OrganisationID_Child]       ,
+            [OrganisationCode_Child]        ,
+            [OrganisationPrimaryRole_Child] ,
+            [RelationshipType]              ,
+            [RelationshipStartDate]         ,
+            [RelationshipEndDate]           ,
+            [Path]                          ,
+            [Depth]                         ,
+            [PathStartDate]                 ,
+            [PathEndDate]                   ,
+            [DateAdded]                     ,
+            [DateUpdated]                   ,
+            o.Organisation_Name             ,
+            1 as isActive  
+        FROM
+            Dictionary.OrganisationDescendent OD JOIN
+            Dictionary.Organisation o on od.SK_OrganisationID_Child = o.SK_OrganisationID
+        WHERE
+            OrganisationCode_Root= 'Y56' 
+            AND OrganisationPrimaryRole_Child in ('RO209','RO172','RO177','RO107','RO99','RO108','RO272','RO261','RO176','RO98','RO96')
+            AND PathStartDate <= GETDATE()
+            AND PathEndDate >= GETDATE()
+            AND RelationshipStartDate <= GETDATE()
+            AND RelationshipEndDate >= GETDATE();
 
-    -- Recursive CTE to calculate hierarchical paths
-    WITH paths(path, Id)
-    AS (
-        -- Handle the root node
-        SELECT 
-            hierarchyid::GetRoot(),
-            id
-        FROM [dbo].[TempOdsInbound]
-        WHERE ParentId IS NULL
+        RAISERROR('Prepare Orgs for Loading',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Prepare Orgs for Loading', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
 
-        UNION ALL
+        -- Attach any 'SELF' relationships to the root node
+        UPDATE [dbo].[TempOdsInbound]
+        SET ParentId = 1, ParentPath = 'ROOT'
+        WHERE RelationshipType = 'SELF';
 
-        -- Handle all other nodes
-        SELECT 
-            CAST(p.path.ToString() + CAST(od.RowNumber AS VARCHAR(30)) + '/' AS hierarchyid), 
-            od.Id 
+        -- Generate readable parent path and assign a parent ID
+        UPDATE [dbo].[TempOdsInbound]
+        SET ParentPath = LEFT(Path, LEN(Path) - LEN(OrganisationCode_Child) - LEN(RelationshipType) - 10)
+        WHERE RelationshipType <> 'SELF';
+
+        -- Assign ParentId using derived parent path
+        UPDATE od
+        SET ParentId = (
+            SELECT id 
+            FROM [dbo].[TempOdsInbound] od1 
+            WHERE od1.path = od.ParentPath
+        )
         FROM [dbo].[TempOdsInbound] od
-        JOIN paths AS p ON od.ParentId = p.Id
-    )
-    UPDATE od
-    SET OdsHierarchy = p.path
-    FROM [dbo].[TempOdsInbound] od
-    JOIN paths p ON od.Id = p.Id;
+        WHERE ParentId IS NULL;
 
-    -- Insert data into OdsDatas
-    INSERT INTO dbo.OdsDatas (Id, OdsHierarchy, OrganisationCode, OrganisationName)
-    SELECT NEWID(), OdsHierarchy, OrganisationCode_Child, ODSOrgName
-    FROM [dbo].[TempOdsInbound]
-    WHERE IsActive = 1;
 
-    -- Mark records with children in OdsDatas
-    UPDATE dbo.OdsDatas
-    SET HasChildren = 1
-    WHERE Id IN (
-        SELECT DISTINCT o1.Id
-        FROM dbo.OdsDatas o1
-        LEFT JOIN dbo.OdsDatas o2 ON o1.OdsHierarchy.GetDescendant(NULL, NULL) = o2.OdsHierarchy
-        WHERE o2.Id IS NOT NULL
-    );
+        RAISERROR('Add in Test Organisation',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Add Test Organisation', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
 
-    -- Commit transaction after all operations are successful
-    COMMIT TRANSACTION;
+        INSERT INTO [dbo].[TempOdsInbound] (ParentPath, ParentId,  OrganisationCode_Child, ODSOrgName)
+        VALUES ('ROOT', 1, 'ZZZ', 'TEST ORG' ) ;
+
+        RAISERROR('Generate hierarchical path',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Generate hierarchical path', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+        -- Assign row numbers for each child
+        UPDATE od
+        SET RowNumber = rn
+        FROM [dbo].[TempOdsInbound] od
+        JOIN (
+            SELECT id, ROW_NUMBER() OVER (PARTITION BY ParentId ORDER BY ParentId) AS rn 
+            FROM [dbo].[TempOdsInbound]
+        ) od1 ON od.id = od1.id;
+
+        -- Recursive CTE to calculate hierarchical paths
+        WITH paths(path, Id)
+        AS (
+            -- Handle the root node
+            SELECT 
+                hierarchyid::GetRoot(),
+                id
+            FROM [dbo].[TempOdsInbound]
+            WHERE ParentId IS NULL
+
+            UNION ALL
+
+            -- Handle all other nodes
+            SELECT 
+                CAST(p.path.ToString() + CAST(od.RowNumber AS VARCHAR(30)) + '/' AS hierarchyid), 
+                od.Id 
+            FROM [dbo].[TempOdsInbound] od
+            JOIN paths AS p ON od.ParentId = p.Id
+        )
+        UPDATE od
+        SET OdsHierarchy = p.path
+        FROM [dbo].[TempOdsInbound] od
+        JOIN paths p ON od.Id = p.Id;
+
+
+        RAISERROR('Create Temp OdsTable',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Create Temp OdsTable', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        CREATE TABLE [dbo].[TempOdsDatas](
+            [Id] [uniqueidentifier] NOT NULL,
+            [RelationshipWithParentStartDate] [datetimeoffset](7) NULL,
+            [RelationshipWithParentEndDate] [datetimeoffset](7) NULL,
+            [OdsHierarchy] [hierarchyid] NULL,
+            [OrganisationCode] [nvarchar](15) NOT NULL,
+            [OrganisationName] [nvarchar](255) NULL,
+            [HasChildren] [bit] NOT NULL
+        ) ON [PRIMARY]
+        ALTER TABLE [dbo].[TempOdsDatas] ADD  DEFAULT (CONVERT([bit],(0))) FOR [HasChildren]
+
+        RAISERROR('Load OdsTable',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Load OdsTable', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        -- Insert data into OdsDatas
+        INSERT INTO dbo.TempOdsDatas (Id, OdsHierarchy, OrganisationCode, OrganisationName)
+        SELECT NEWID(), OdsHierarchy, OrganisationCode_Child, ODSOrgName
+        FROM [dbo].[TempOdsInbound]
+        WHERE IsActive = 1;
+
+        -- Mark records with children in OdsDatas
+        UPDATE dbo.TempOdsDatas
+        SET HasChildren = 1
+        WHERE Id IN (
+            SELECT DISTINCT o1.Id
+            FROM dbo.TempOdsDatas o1
+            LEFT JOIN dbo.TempOdsDatas o2 ON o1.OdsHierarchy.GetDescendant(NULL, NULL) = o2.OdsHierarchy
+            WHERE o2.Id IS NOT NULL
+        );
+
+        RAISERROR('Create Primary Key',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Create Primary Key', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        DECLARE @SQL NVARCHAR(MAX) = 'ALTER TABLE [dbo].[TempOdsDatas] ADD  CONSTRAINT [PK_OdsDatas_' + CAST(NEWID() as nvarchar(36)) + '] PRIMARY KEY CLUSTERED ' +
+                            '([Id] ASC)' + 
+                            'WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ' +
+                            'ON [PRIMARY]'
+        PRINT @SQL
+        EXEC sp_executesql @SQL
+
+        RAISERROR('Swapping Live and Temp Table',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Swapping Live and Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        DECLARE @BackupSqlName NVARCHAR(MAX) = 'OdsDatasBak_' + FORMAT(GETUTCDATE(),'yyyyMMddHHmmss')
+        exec sp_rename 'dbo.OdsDatas',  @BackupSqlName
+        exec sp_rename 'dbo.TempOdsDatas', 'OdsDatas'
+
+        IF OBJECT_ID('dbo.TempOdsInbound') IS NOT NULL
+        BEGIN
+            RAISERROR('Dropping Temp Table',0,1) WITH NOWAIT;
+            
+            INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+            VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Dropping Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+            DROP TABLE [dbo].[TempOdsInbound]
+        END;
+
+        RAISERROR('Load Complete',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Load Complete', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+    END TRY
+    BEGIN CATCH
+
+        DECLARE @ErrorMessage NVARCHAR(4000);
+        SET @ErrorMessage = 'ODSLoad Load Failed: ' + ERROR_MESSAGE();
+
+        RAISERROR(@ErrorMessage,0,1) WITH NOWAIT;
+
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'ODSLoad', @ErrorMessage, 'Error', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+        IF OBJECT_ID('dbo.TempOdsDatas') IS NOT NULL
+        BEGIN
+            RAISERROR('Dropping Temp Table',0,1) WITH NOWAIT;
+            
+            INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+            VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Dropping Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+            DROP TABLE [dbo].[TempOdsDatas]
+        END;
+
+        IF OBJECT_ID('dbo.TempOdsInbound') IS NOT NULL
+        BEGIN
+            RAISERROR('Dropping Temp Table',0,1) WITH NOWAIT;
+            
+            INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+            VALUES (NEWID(), @CorrelationId, 'ODSLoad', 'Dropping Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+            DROP TABLE [dbo].[TempOdsInbound]
+        END;
+
+        THROW;
+    END CATCH
 END;
+GO
--- ISL.ReIdentification.Core/Brokers/Storages/Sql/ReIdentifications/Scripts/CreateLoadPdsDataStoredProcedure.sql
+++ ISL.ReIdentification.Core/Brokers/Storages/Sql/ReIdentifications/Scripts/CreateLoadPdsDataStoredProcedure.sql
@@ -1,33 +1,137 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[LoadPDSData]
 AS
 BEGIN
-    -- Check if the temporary table exists
-    IF OBJECT_ID('dbo.TempPdsInbound') IS NULL
-        THROW 50000, 'Temp Table does not exist.', 1;
+    DECLARE @CorrelationId UNIQUEIDENTIFIER = NEWID();
+    
+    BEGIN TRY 
+        -- Check if the temporary table exists
+        IF OBJECT_ID('pds.PDS_PATIENT_CARE_PRACTICE') IS NULL
+            THROW 50000, 'Inbound Table does not exist.', 1;
 
-    -- Check if the temporary table has data
-    IF (SELECT COUNT(*) FROM dbo.TempPdsInbound) = 0
-        THROW 50000, 'Temp Table is empty.', 1;
+        -- Check if the temporary table has data
+        IF (SELECT COUNT(*) FROM pds.PDS_PATIENT_CARE_PRACTICE) = 0
+            THROW 50000, 'Inbound Table is empty.', 1;
 
-    -- Begin transaction for data insertion
-    BEGIN TRANSACTION;
+        IF OBJECT_ID('dbo.TempPdsDatas') IS NOT NULL
+            THROW 50000, 'Temp Table already exists.', 1;
 
-    -- Clear existing data in PDSDatas table
-    TRUNCATE TABLE dbo.PDSDatas;
+        RAISERROR('Load Commenced',0,1) WITH NOWAIT;
+    
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Load Commenced', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
 
-    -- Insert new data from the temporary table with unpivoted columns
-    INSERT INTO dbo.PDSDatas (PseudoNhsNumber, OrgCode, ID)
-    SELECT [Pseudo NHS Number], OdsCode, NEWID()
-    FROM (
-        SELECT 
-            [Pseudo NHS Number],
-            [Primary Care Provider],
-            CAST([derCurrentCcgOfRegistration] AS VARCHAR(8)) AS [derCurrentCcgOfRegistration],
-            CAST([derCurrentIcbOfRegistration] AS VARCHAR(8)) AS [derCurrentIcbOfRegistration]
-        FROM dbo.TempPdsInbound
-    ) AS SourceTable
-    UNPIVOT (OdsCode FOR ODSRel IN ([Primary Care Provider], [derCurrentCcgOfRegistration], [derCurrentIcbOfRegistration])) AS Unpiv;
+        CREATE TABLE [dbo].[TempPdsDatas](
+            [PseudoNhsNumber] [nvarchar](10) NOT NULL,
+            [RelationshipWithOrganisationEffectiveFromDate] [datetimeoffset](7) NULL,
+            [RelationshipWithOrganisationEffectiveToDate] [datetimeoffset](7) NULL,
+            [Id] [uniqueidentifier] NOT NULL,
+            [OrgCode] [nvarchar](15) NOT NULL,
+            [OrganisationName] [nvarchar](max) NULL
+        ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-    -- Commit transaction after successful insertion
-    COMMIT TRANSACTION;
+        RAISERROR('Load Temp Table Created',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Load Temp Table Created', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        RAISERROR('Load Commenced into Temp Table',0,1) WITH NOWAIT;
+        -- Begin transaction for data insertion
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Load Commenced into Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        -- Insert new data from the temporary table with unpivoted columns
+        INSERT INTO dbo.TempPdsDatas (PseudoNhsNumber, OrgCode, ID)
+        (SELECT 
+            [PseudoNhsNumber],
+            [OrgCode],
+            NEWID()
+        FROM pds.PDS_PATIENT_CARE_PRACTICE
+        where OrgCode is not null and PseudoNhsNumber is not null)
+
+        RAISERROR('Temp Table Load Completed',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Temp Table Load Completed', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        RAISERROR('Creating Primary Keys',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Creating Primary Keys', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+        
+        DECLARE @SQL NVARCHAR(MAX) = 'ALTER TABLE [dbo].[TempPdsDatas] ADD  CONSTRAINT [PK_PdsDatas_' + CAST(NEWID() as nvarchar(36)) + '] PRIMARY KEY CLUSTERED' +
+                                    '([Id] ASC)' + 
+                                    'WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)' +
+                                    'ON [PRIMARY]'
+        PRINT @SQL
+        EXEC sp_executesql @SQL
+
+        RAISERROR('Creating Pseudo Id Index',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Creating Pseudo Id Index', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+        
+        CREATE NONCLUSTERED INDEX [IX_PdsDatas_PseudoNhsNumber] ON [dbo].[TempPdsDatas]
+        (
+            [PseudoNhsNumber] ASC
+        )WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+        RAISERROR('Creating Ods code Index',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Creating Ods code Index', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        CREATE NONCLUSTERED INDEX [IX_PdsDatas_OrgCode] ON [dbo].[TempPdsDatas]
+        (
+            [OrgCode] ASC
+        )WITH (STATISTICS_NORECOMPUTE = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+        RAISERROR('Adding Defaults',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Adding Defaults', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        ALTER TABLE [dbo].[TempPdsDatas] ADD  DEFAULT (N'') FOR [PseudoNhsNumber]
+
+        ALTER TABLE [dbo].[TempPdsDatas] ADD  DEFAULT ('00000000-0000-0000-0000-000000000000') FOR [Id]
+
+        ALTER TABLE [dbo].[TempPdsDatas] ADD  DEFAULT (N'') FOR [OrgCode]
+
+        RAISERROR('Adding Test Records - TODO',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Adding Test Records', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        RAISERROR('Swapping Live and Temp Table',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Swapping Live and Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+
+        DECLARE @BackupSqlName NVARCHAR(MAX) = 'PDSDatasBak_' + FORMAT(GETUTCDATE(),'yyyyMMddHHmmss')
+        exec sp_rename 'dbo.PDSDatas',  @BackupSqlName
+        exec sp_rename 'dbo.TempPdsDatas', 'PDSDatas'
+
+        RAISERROR('Load Completed',0,1) WITH NOWAIT;
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Load Completed', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE())
+    END TRY
+    BEGIN CATCH
+
+        DECLARE @ErrorMessage NVARCHAR(4000);
+        SET @ErrorMessage = 'PDS Load Failed: ' + ERROR_MESSAGE();
+
+        RAISERROR(@ErrorMessage,0,1) WITH NOWAIT;
+
+        INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+        VALUES (NEWID(), @CorrelationId, 'PDSLoad', @ErrorMessage, 'Error', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+        IF OBJECT_ID('dbo.TempPdsDatas') IS NOT NULL
+        BEGIN
+            RAISERROR('Dropping Temp Table',0,1) WITH NOWAIT;
+            
+            INSERT INTO Audits (id, CorrelationId, AuditType, AuditDetail, LogLevel, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate ) 
+            VALUES (NEWID(), @CorrelationId, 'PDSLoad', 'Dropping Temp Table', 'Info', CURRENT_USER, GETUTCDATE(),  CURRENT_USER, GETUTCDATE());
+
+            DROP TABLE [dbo].[TempPdsDatas]
+        END;
+
+        THROW;
+    END CATCH
 END;
+GO
+
