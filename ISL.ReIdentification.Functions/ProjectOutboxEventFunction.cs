759c0cea223b31c20f2d225a6260f6409a448f3e
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-03-07 15:52:10 +0000

    WIP -> Testing HttpTrigger

--- ISL.ReIdentification.Functions/ProjectOutboxEventFunction.cs
+++ ISL.ReIdentification.Functions/ProjectOutboxEventFunction.cs
@@ -2,8 +2,16 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Services.Coordinations.Identifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace ISL.ReIdentification.Functions
 {
@@ -20,25 +28,47 @@ namespace ISL.ReIdentification.Functions
             this.loggingBroker = loggingBroker;
         }
 
-        //[Function("ProjectOutboxEventFunction")]
-        //public async Task Run(
-        //    [BlobTrigger("addresses/resolve/in/{name}", Connection = "BlobStorage")] Stream myBlob, string name)
-        //{
-        //    await loggingBroker
-        //        .LogInformationAsync(
-        //            $"C# Blob trigger function Processing blob\n " +
-        //            $"Name: address/in/{{name}}");
+        [Function("ProjectOutboxEventFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        {
+            await loggingBroker
+                .LogInformationAsync(
+                    $"C# Blob trigger function Processing blob\n " +
+                    $"Name: address/in/{{name}}");
 
-        //    try
-        //    {
-        //        // TODO:  Refactor the identification service to take in the stream and name
-        //        // await this.identificationCoordinationService.ProcessImpersonationContextRequestAsync(name, myBlob);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        await loggingBroker.LogErrorAsync(ex);
-        //        throw;
-        //    }
-        //}
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+                EventGridEvent[] egEvents = EventGridEvent.ParseMany(BinaryData.FromString(requestBody));
+
+                //var eventGridEvent = JArray.Parse(requestBody)[0];
+                //string subject = eventGridEvent["subject"]?.ToString();
+                //string container = subject?.Split('/')[4];
+                //string filename = subject?.Split(new[] { "/blobs/" }, StringSplitOptions.None)[1];
+                foreach (EventGridEvent egEvent in egEvents)
+                {
+                    if (egEvent.EventType == "Microsoft.EventGrid.SubscriptionValidationEvent")
+                    {
+                        var data = egEvent.Data.ToObjectFromJson<SubscriptionValidationEventData>();
+
+                        SubscriptionValidationResponse response = new SubscriptionValidationResponse()
+                        {
+                            ValidationResponse = data.ValidationCode
+                        };
+
+                        return new OkObjectResult(response);
+                    }
+                }
+
+                return new OkObjectResult(string.Empty);
+            }
+            catch (Exception ex)
+            {
+                await loggingBroker.LogErrorAsync(ex);
+                throw;
+            }
+        }
     }
 }
