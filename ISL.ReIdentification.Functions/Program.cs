fde7aa8f94bb9c0984b6d44eecfc93c6fdba14f0
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-03-10 13:36:26 +0000

    CODE RUB: Add missing services to Program.cs

--- ISL.ReIdentification.Functions/Program.cs
+++ ISL.ReIdentification.Functions/Program.cs
@@ -5,16 +5,12 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using Azure.Core;
-using Azure.Identity;
 using ISL.Providers.Notifications.Abstractions;
 using ISL.Providers.Notifications.GovukNotify.Models;
 using ISL.Providers.Notifications.GovukNotify.Providers.Notifications;
 using ISL.Providers.ReIdentification.Abstractions;
 using ISL.Providers.ReIdentification.Necs.Models.Brokers.NECS;
 using ISL.Providers.ReIdentification.Necs.Providers.NecsReIdentifications;
-using ISL.Providers.ReIdentification.OfflineFileSources.Models;
-using ISL.Providers.ReIdentification.OfflineFileSources.Providers.OfflineFileSources;
 using ISL.Providers.Storages.Abstractions;
 using ISL.Providers.Storages.AzureBlobStorage.Models;
 using ISL.Providers.Storages.AzureBlobStorage.Providers.AzureBlobStorage;
@@ -24,7 +20,9 @@ using ISL.ReIdentification.Core.Brokers.Hashing;
 using ISL.ReIdentification.Core.Brokers.Identifiers;
 using ISL.ReIdentification.Core.Brokers.Loggings;
 using ISL.ReIdentification.Core.Brokers.Notifications;
+using ISL.ReIdentification.Core.Brokers.ReIdentifications;
 using ISL.ReIdentification.Core.Brokers.Securities;
+using ISL.ReIdentification.Core.Brokers.Storages.Blob;
 using ISL.ReIdentification.Core.Brokers.Storages.Sql.ReIdentifications;
 using ISL.ReIdentification.Core.Models.Brokers.Notifications;
 using ISL.ReIdentification.Core.Models.Coordinations.Identifications;
@@ -32,9 +30,19 @@ using ISL.ReIdentification.Core.Models.Orchestrations.Persists;
 using ISL.ReIdentification.Core.Services.Coordinations.Identifications;
 using ISL.ReIdentification.Core.Services.Foundations.AccessAudits;
 using ISL.ReIdentification.Core.Services.Foundations.CsvIdentificationRequests;
+using ISL.ReIdentification.Core.Services.Foundations.Documents;
 using ISL.ReIdentification.Core.Services.Foundations.ImpersonationContexts;
+using ISL.ReIdentification.Core.Services.Foundations.Lookups;
 using ISL.ReIdentification.Core.Services.Foundations.Notifications;
+using ISL.ReIdentification.Core.Services.Foundations.OdsDatas;
+using ISL.ReIdentification.Core.Services.Foundations.PdsDatas;
+using ISL.ReIdentification.Core.Services.Foundations.ReIdentifications;
+using ISL.ReIdentification.Core.Services.Foundations.UserAccesses;
+using ISL.ReIdentification.Core.Services.Foundations.UserAgreements;
+using ISL.ReIdentification.Core.Services.Orchestrations.Accesses;
+using ISL.ReIdentification.Core.Services.Orchestrations.Identifications;
 using ISL.ReIdentification.Core.Services.Orchestrations.Persists;
+using ISL.ReIdentification.Core.Services.Processings.UserAccesses;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -79,11 +87,11 @@ internal class Program
             services.AddSingleton(csvReIdentificationConfigurations);
 
             AddProviders(services, configuration);
-            AddBrokers(services);
-            AddServices(services);
-            AddProcessings(services);
-            AddOrchestrations(services);
-            AddCoordinations(services);
+            AddBrokers(services, configuration);
+            AddFoundationServices(services);
+            AddProcessingServices(services);
+            AddOrchestrationServices(services, configuration);
+            AddCoordinationServices(services, configuration);
         })
         .UseDefaultServiceProvider(options => options.ValidateScopes = false)
         .ConfigureFunctionsWorkerDefaults()
@@ -126,63 +134,72 @@ internal class Program
         bool reIdentificationProviderOfflineMode = configuration
             .GetSection("ReIdentificationProviderOfflineMode").Get<bool>();
 
-        if (reIdentificationProviderOfflineMode == true)
-        {
-            OfflineSourceReIdentificationConfigurations offlineSourceReIdentificationConfigurations = configuration
-                .GetSection("OfflineSourceReIdentificationConfigurations")
-                    .Get<OfflineSourceReIdentificationConfigurations>();
+        NecsReIdentificationConfigurations necsReIdentificationConfigurations = configuration
+            .GetSection("NecsReIdentificationConfigurations")
+                .Get<NecsReIdentificationConfigurations>();
 
-            services.AddSingleton(offlineSourceReIdentificationConfigurations);
-            services.AddTransient<IReIdentificationProvider, OfflineFileSourceReIdentificationProvider>();
-        }
-        else
-        {
-            NecsReIdentificationConfigurations necsReIdentificationConfigurations = configuration
-                .GetSection("NecsReIdentificationConfigurations")
-                    .Get<NecsReIdentificationConfigurations>();
-
-            services.AddSingleton(necsReIdentificationConfigurations);
-            services.AddTransient<IReIdentificationProvider, NecsReIdentificationProvider>();
-        }
-
+        services.AddSingleton(necsReIdentificationConfigurations);
+        services.AddTransient<IReIdentificationProvider, NecsReIdentificationProvider>();
         services.AddTransient<IReIdentificationAbstractionProvider, ReIdentificationAbstractionProvider>();
     }
 
-    private static void AddBrokers(IServiceCollection services)
+    private static void AddBrokers(IServiceCollection services, IConfiguration configuration)
     {
-        var credential = new DefaultAzureCredential();
-        var tokenRequestContext = new TokenRequestContext(new[] { "https://graph.microsoft.com/.default" });
-        AccessToken accessToken = credential.GetTokenAsync(tokenRequestContext).Result;
-        SecurityBroker securityBroker = new SecurityBroker(accessToken.Token);
-
-        services.AddTransient<ISecurityBroker>(broker => securityBroker);
-        services.AddTransient<ILoggingBroker, LoggingBroker>();
         services.AddTransient<IDateTimeBroker, DateTimeBroker>();
         services.AddTransient<IIdentifierBroker, IdentifierBroker>();
-        services.AddTransient<IReIdentificationStorageBroker, ReIdentificationStorageBroker>();
-        services.AddTransient<IHashBroker, HashBroker>();
-        services.AddTransient<INotificationBroker, NotificationBroker>();
+        services.AddTransient<ILoggingBroker, LoggingBroker>();
         services.AddTransient<ICsvHelperBroker, CsvHelperBroker>();
+        services.AddTransient<ISecurityBroker, SecurityBroker>();
+        services.AddTransient<IReIdentificationStorageBroker, ReIdentificationStorageBroker>();
+        services.AddTransient<INotificationBroker, NotificationBroker>();
+        services.AddTransient<IHashBroker, HashBroker>();
+        services.AddTransient<IBlobStorageBroker, BlobStorageBroker>();
+        services.AddSingleton<IReIdentificationBroker, ReIdentificationBroker>();
     }
 
-    private static void AddServices(IServiceCollection services)
+    private static void AddFoundationServices(IServiceCollection services)
     {
+        services.AddTransient<IAccessAuditService, AccessAuditService>();
+        services.AddTransient<IImpersonationContextService, ImpersonationContextService>();
+        services.AddTransient<ILookupService, LookupService>();
+        services.AddTransient<IOdsDataService, OdsDataService>();
+        services.AddTransient<IPdsDataService, PdsDataService>();
+        services.AddTransient<IUserAccessService, UserAccessService>();
         services.AddTransient<IImpersonationContextService, ImpersonationContextService>();
         services.AddTransient<ICsvIdentificationRequestService, CsvIdentificationRequestService>();
         services.AddTransient<INotificationService, NotificationService>();
-        services.AddTransient<IAccessAuditService, AccessAuditService>();
+        services.AddTransient<IReIdentificationService, ReIdentificationService>();
+        services.AddTransient<IDocumentService, DocumentService>();
+        services.AddTransient<IUserAgreementService, UserAgreementService>();
     }
 
-    private static void AddProcessings(IServiceCollection services)
-    { }
-
-    private static void AddOrchestrations(IServiceCollection services)
+    private static void AddProcessingServices(IServiceCollection services)
     {
+        services.AddTransient<IUserAccessProcessingService, UserAccessProcessingService>();
+    }
+
+    private static void AddOrchestrationServices(IServiceCollection services, IConfiguration configuration)
+    {
+        CsvReIdentificationConfigurations csvReIdentificationConfigurations = configuration
+            .GetSection("CsvReIdentificationConfigurations")
+                .Get<CsvReIdentificationConfigurations>() ??
+                    new CsvReIdentificationConfigurations();
+
+        services.AddSingleton(csvReIdentificationConfigurations);
+        services.AddTransient<IAccessOrchestrationService, AccessOrchestrationService>();
         services.AddTransient<IPersistanceOrchestrationService, PersistanceOrchestrationService>();
+        services.AddTransient<IIdentificationOrchestrationService, IdentificationOrchestrationService>();
+        services.AddTransient<ICsvIdentificationRequestService, CsvIdentificationRequestService>();
     }
 
-    private static void AddCoordinations(IServiceCollection services)
+    private static void AddCoordinationServices(IServiceCollection services, IConfiguration configuration)
     {
+        ProjectStorageConfiguration projectStorageConfiguration = configuration
+            .GetSection("ProjectStorageConfiguration")
+                .Get<ProjectStorageConfiguration>();
+
+        services.AddSingleton(projectStorageConfiguration);
+
         services.AddTransient<IIdentificationCoordinationService, IdentificationCoordinationService>();
     }
 }
