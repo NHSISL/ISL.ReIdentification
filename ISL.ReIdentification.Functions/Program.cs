87e2205ecbe8caa88698321f1efedc69751fa62b
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2025-03-10 14:29:02 +0000

    CODE RUB: Remove offline provider configuration

--- ISL.ReIdentification.Functions/Program.cs
+++ ISL.ReIdentification.Functions/Program.cs
@@ -13,8 +13,6 @@ using ISL.Providers.Notifications.GovukNotify.Providers.Notifications;
 using ISL.Providers.ReIdentification.Abstractions;
 using ISL.Providers.ReIdentification.Necs.Models.Brokers.NECS;
 using ISL.Providers.ReIdentification.Necs.Providers.NecsReIdentifications;
-using ISL.Providers.ReIdentification.OfflineFileSources.Models;
-using ISL.Providers.ReIdentification.OfflineFileSources.Providers.OfflineFileSources;
 using ISL.Providers.Storages.Abstractions;
 using ISL.Providers.Storages.AzureBlobStorage.Models;
 using ISL.Providers.Storages.AzureBlobStorage.Providers.AzureBlobStorage;
@@ -123,28 +121,12 @@ internal class Program
         services.AddTransient<INotificationProvider, GovukNotifyProvider>();
         services.AddTransient<IStorageProvider, AzureBlobStorageProvider>();
 
-        bool reIdentificationProviderOfflineMode = configuration
-            .GetSection("ReIdentificationProviderOfflineMode").Get<bool>();
+        NecsReIdentificationConfigurations necsReIdentificationConfigurations = configuration
+            .GetSection("NecsReIdentificationConfigurations")
+                .Get<NecsReIdentificationConfigurations>();
 
-        if (reIdentificationProviderOfflineMode == true)
-        {
-            OfflineSourceReIdentificationConfigurations offlineSourceReIdentificationConfigurations = configuration
-                .GetSection("OfflineSourceReIdentificationConfigurations")
-                    .Get<OfflineSourceReIdentificationConfigurations>();
-
-            services.AddSingleton(offlineSourceReIdentificationConfigurations);
-            services.AddTransient<IReIdentificationProvider, OfflineFileSourceReIdentificationProvider>();
-        }
-        else
-        {
-            NecsReIdentificationConfigurations necsReIdentificationConfigurations = configuration
-                .GetSection("NecsReIdentificationConfigurations")
-                    .Get<NecsReIdentificationConfigurations>();
-
-            services.AddSingleton(necsReIdentificationConfigurations);
-            services.AddTransient<IReIdentificationProvider, NecsReIdentificationProvider>();
-        }
-
+        services.AddSingleton(necsReIdentificationConfigurations);
+        services.AddTransient<IReIdentificationProvider, NecsReIdentificationProvider>();
         services.AddTransient<IReIdentificationAbstractionProvider, ReIdentificationAbstractionProvider>();
     }
 
